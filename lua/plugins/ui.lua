b8308d0caeab9a0cbeddd3b2e1a77885e96b0286
root <root@Asus.>
2023-12-25 23:44:36 +0000

    noice config, cleanup, removed inednt-blankline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -186,17 +186,7 @@ return {
         },
         format_item_override = {}, -- Used to override format_item. See :help dressing-format
         -- see :help dressing_get_config
-        -- get_config = function(opts)
-        --   if opts.kind == 'codeaction' then
-        --     return {
-        --       backend = 'nui',
-        --       nui = {
-        --         relative = 'cursor',
-        --         max_width = 40,
-        --       },
-        --     }
-        --   end
-        -- end,
+        get_config = nil,
       },
     },
     init = function()
@@ -345,16 +335,6 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     event = 'VeryLazy',
-    -- init = function()
-    --   vim.g.lualine_laststatus = vim.o.laststatus
-    --   if vim.fn.argc(-1) > 0 then
-    --     -- set an empty statusline till lualine loads
-    --     vim.o.statusline = ' '
-    --   else
-    --     -- hide the statusline on the starter page
-    --     vim.o.laststatus = 0
-    --   end
-    -- end,
     config = function(_, opts)
       require('lualine').setup(opts)
     end,
@@ -481,46 +461,56 @@ return {
     end,
   },
 
-  -- indent guides for Neovim
-  {
-    'lukas-reineke/indent-blankline.nvim',
-    event = 'VeryLazy',
-    opts = {
-      indent = {
-        char = '│',
-        tab_char = '│',
-      },
-      scope = { enabled = false },
-      exclude = {
-        filetypes = {
-          'help',
-          'alpha',
-          'dashboard',
-          'neo-tree',
-          'Trouble',
-          'trouble',
-          'lazy',
-          'mason',
-          'notify',
-          'toggleterm',
-          'lazyterm',
-        },
-      },
-    },
-    main = 'ibl',
-  },
-
   -- Active indent guide and indent text objects. When you're browsing
   -- code, this highlights the current level of indentation, and animates
   -- the highlighting.
+  -- see: `:h mini.indentscope`
   {
     'echasnovski/mini.indentscope',
     version = false, -- wait till new 0.7.0 release to put it back on semver
     event = 'VeryLazy',
     opts = {
-      -- symbol = "▏",
-      symbol = '│',
-      options = { try_as_border = true },
+      draw = {
+        -- Delay (in ms) between event and start of drawing scope indicator
+        delay = 50,
+
+        -- Animation rule for scope's first drawing. A function which, given
+        -- next and total step numbers, returns wait time (in ms). See
+        -- |MiniIndentscope.gen_animation| for builtin options. To disable
+        -- animation, use `require('mini.indentscope').gen_animation.none()`.
+        -- animation = --<function: implements constant 20ms between steps>,
+
+        -- Symbol priority. Increase to display on top of more symbols.
+        priority = 2,
+      },
+
+      -- Module mappings. Use `''` (empty string) to disable one.
+      mappings = {
+        -- Textobjects
+        object_scope = 'ii',
+        object_scope_with_border = 'ai',
+
+        -- Motions (jump to respective border line; if not present - body line)
+        goto_top = '[i',
+        goto_bottom = ']i',
+      },
+
+      -- Options which control scope computation
+      options = {
+        -- Type of scope's border: which line(s) with smaller indent to
+        -- categorize as border. Can be one of: 'both', 'top', 'bottom', 'none'.
+        border = 'both',
+
+        -- Whether to use cursor column when computing reference indent.
+        -- Useful to see incremental scopes with horizontal cursor movements.
+        indent_at_cursor = true,
+
+        -- Whether to first check input line to be a border of adjacent scope.
+        -- Use it if you want to place cursor on function header to get scope of
+        -- its body.
+        try_as_border = true,
+      },
+      symbol = '│', -- Which character to use for drawing scope indicator
     },
     init = function()
       vim.api.nvim_create_autocmd('FileType', {
@@ -558,14 +548,207 @@ return {
   {
     'folke/noice.nvim',
     event = 'VeryLazy',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    },
     opts = {
-      lsp = {
-        override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-          ['vim.lsp.util.stylize_markdown'] = true,
-          ['cmp.entry.get_documentation'] = true,
+      cmdline = {
+        enabled = true, -- enables the Noice cmdline UI
+        view = 'cmdline_popup', -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+        opts = {}, -- global options for the cmdline. See section on views
+        ---@type table<string, CmdlineFormat>
+        format = {
+          -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+          -- view: (default is cmdline view)
+          -- opts: any options passed to the view
+          -- icon_hl_group: optional hl_group for the icon
+          -- title: set to anything or empty string to hide
+          cmdline = { pattern = '^:', icon = '', lang = 'vim' },
+          search_down = { kind = 'search', pattern = '^/', icon = ' ', lang = 'regex' },
+          search_up = { kind = 'search', pattern = '^%?', icon = ' ', lang = 'regex' },
+          filter = { pattern = '^:%s*!', icon = '$', lang = 'bash' },
+          lua = { pattern = { '^:%s*lua%s+', '^:%s*lua%s*=%s*', '^:%s*=%s*' }, icon = '', lang = 'lua' },
+          help = { pattern = '^:%s*he?l?p?%s+', icon = '' },
+          input = {}, -- Used by input()
+          -- lua = false, -- to disable a format, set to `false`
         },
       },
+      messages = {
+        -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+        -- This is a current Neovim limitation.
+        enabled = true, -- enables the Noice messages UI
+        view = 'notify', -- default view for messages
+        view_error = 'notify', -- view for errors
+        view_warn = 'notify', -- view for warnings
+        view_history = 'messages', -- view for :messages
+        view_search = 'virtualtext', -- view for search count messages. Set to `false` to disable
+      },
+      popupmenu = {
+        enabled = true, -- enables the Noice popupmenu UI
+        ---@type 'nui'|'cmp'
+        backend = 'nui', -- backend to use to show regular cmdline completions
+        ---@type NoicePopupmenuItemKind|false
+        -- Icons for completion item kinds (see defaults at noice.config.icons.kinds)
+        kind_icons = {}, -- set to `false` to disable icons
+      },
+      -- default options for require('noice').redirect
+      -- see the section on Command Redirection
+      ---@type NoiceRouteConfig
+      redirect = {
+        view = 'popup',
+        filter = { event = 'msg_show' },
+      },
+      -- You can add any custom commands below that will be available with `:Noice command`
+      ---@type table<string, NoiceCommand>
+      commands = {
+        history = {
+          -- options for the message history that you get with `:Noice`
+          view = 'popup',
+          opts = { enter = true, format = 'details' },
+          filter = {
+            any = {
+              { event = 'notify' },
+              { error = true },
+              { warning = true },
+              { event = 'msg_show', kind = { '' } },
+              { event = 'lsp', kind = 'message' },
+            },
+          },
+        },
+        -- :Noice last
+        last = {
+          view = 'popup',
+          opts = { enter = true, format = 'details' },
+          filter = {
+            any = {
+              { event = 'notify' },
+              { error = true },
+              { warning = true },
+              { event = 'msg_show', kind = { '' } },
+              { event = 'lsp', kind = 'message' },
+            },
+          },
+          filter_opts = { count = 1 },
+        },
+        -- :Noice errors
+        errors = {
+          -- options for the message history that you get with `:Noice`
+          view = 'popup',
+          opts = { enter = true, format = 'details' },
+          filter = { error = true },
+          filter_opts = { reverse = true },
+        },
+      },
+      notify = {
+        -- Noice can be used as `vim.notify` so you can route any notification like other messages
+        -- Notification messages have their level and other properties set.
+        -- event is always "notify" and kind can be any log level as a string
+        -- The default routes will forward notifications to nvim-notify
+        -- Benefit of using Noice for this is the routing and consistent history view
+        enabled = true,
+        view = 'notify',
+      },
+      lsp = {
+        progress = {
+          enabled = true,
+          -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
+          -- See the section on formatting for more details on how to customize.
+          --- @type NoiceFormat|string
+          format = 'lsp_progress',
+          --- @type NoiceFormat|string
+          format_done = 'lsp_progress_done',
+          throttle = 1000 / 30, -- frequency to update lsp progress message
+          view = 'mini',
+        },
+        override = {
+          -- override the default lsp markdown formatter with Noice
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          -- override the lsp markdown formatter with Noice
+          ['vim.lsp.util.stylize_markdown'] = true,
+          -- override cmp documentation with Noice (needs the other options to work)
+          ['cmp.entry.get_documentation'] = true,
+        },
+        hover = {
+          enabled = true,
+          silent = false, -- set to true to not show a message if hover is not available
+          view = nil, -- when nil, use defaults from documentation
+          ---@type NoiceViewOptions
+          opts = {}, -- merged with defaults from documentation
+        },
+        signature = {
+          enabled = true,
+          auto_open = {
+            enabled = true,
+            trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+            luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+            throttle = 50, -- Debounce lsp signature help request by 50ms
+          },
+          view = nil, -- when nil, use defaults from documentation
+          ---@type NoiceViewOptions
+          opts = {}, -- merged with defaults from documentation
+        },
+        message = {
+          -- Messages shown by lsp servers
+          enabled = true,
+          view = 'notify',
+          opts = {},
+        },
+        -- defaults for hover and signature help
+        documentation = {
+          view = 'hover',
+          ---@type NoiceViewOptions
+          opts = {
+            lang = 'markdown',
+            replace = true,
+            render = 'plain',
+            format = { '{message}' },
+            win_options = { concealcursor = 'n', conceallevel = 3 },
+          },
+        },
+      },
+      markdown = {
+        hover = {
+          ['|(%S-)|'] = vim.cmd.help, -- vim help links
+          -- ['%[.-%]%((%S-)%)'] = require('noice.util').open, -- markdown links
+        },
+        highlights = {
+          ['|%S-|'] = '@text.reference',
+          ['@%S+'] = '@parameter',
+          ['^%s*(Parameters:)'] = '@text.title',
+          ['^%s*(Return:)'] = '@text.title',
+          ['^%s*(See also:)'] = '@text.title',
+          ['{%S-}'] = '@parameter',
+        },
+      },
+      health = {
+        checker = true, -- Disable if you don't want health checks to run
+      },
+      smart_move = {
+        -- noice tries to move out of the way of existing floating windows.
+        enabled = true, -- you can disable this behaviour here
+        -- add any filetypes here, that shouldn't trigger smart move.
+        excluded_filetypes = { 'cmp_menu', 'cmp_docs', 'notify' },
+      },
+      ---@type NoicePresets
+      presets = {
+        -- you can enable a preset by setting it to true, or a table that will override the preset config
+        -- you can also add custom presets that you can enable/disable with enabled=true
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = true, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+      throttle = 1000 / 30, -- how frequently does Noice need to check for ui updates? This has no effect when in blocking mode.
+      ---@type NoiceConfigViews
+      views = {}, ---@see section on views
+      ---@type NoiceRouteConfig[]
+      ---@type table<string, NoiceFilter>
+      status = {}, --- @see section on statusline components
+      ---@type NoiceFormatOptions
+      format = {}, --- @see section on formatting
+
       routes = {
         {
           filter = {
@@ -579,30 +762,76 @@ return {
           view = 'mini',
         },
       },
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        inc_rename = true,
-      },
     },
-    -- stylua: ignore
     keys = {
-      { "<S-Enter>",   function() require("noice").redirect(vim.fn.getcmdline()) end,                 mode = "c",                 desc = "Redirect Cmdline" },
-      { "<leader>snl", function() require("noice").cmd("last") end,                                   desc = "Noice Last Message" },
-      { "<leader>snh", function() require("noice").cmd("history") end,                                desc = "Noice History" },
-      { "<leader>sna", function() require("noice").cmd("all") end,                                    desc = "Noice All" },
-      { "<leader>snd", function() require("noice").cmd("dismiss") end,                                desc = "Dismiss All" },
-      { "<c-f>",       function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end,  silent = true,              expr = true,              desc = "Scroll forward",  mode = { "i", "n", "s" } },
-      { "<c-b>",       function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true,              expr = true,              desc = "Scroll backward", mode = { "i", "n", "s" } },
+      {
+        '<S-Enter>',
+        function()
+          require('noice').redirect(vim.fn.getcmdline())
+        end,
+        mode = 'c',
+        desc = 'Redirect Cmdline',
+      },
+      {
+        '<leader>snl',
+        function()
+          require('noice').cmd 'last'
+        end,
+        desc = 'Noice Last Message',
+      },
+      {
+        '<leader>snh',
+        function()
+          require('noice').cmd 'history'
+        end,
+        desc = 'Noice History',
+      },
+      {
+        '<leader>sna',
+        function()
+          require('noice').cmd 'all'
+        end,
+        desc = 'Noice All',
+      },
+      {
+        '<leader>snd',
+        function()
+          require('noice').cmd 'dismiss'
+        end,
+        desc = 'Dismiss All',
+      },
+      {
+        '<c-f>',
+        function()
+          if not require('noice.lsp').scroll(4) then
+            return '<c-f>'
+          end
+        end,
+        silent = true,
+        expr = true,
+        desc = 'Scroll forward',
+        mode = { 'i', 'n', 's' },
+      },
+      {
+        '<c-b>',
+        function()
+          if not require('noice.lsp').scroll(-4) then
+            return '<c-b>'
+          end
+        end,
+        silent = true,
+        expr = true,
+        desc = 'Scroll backward',
+        mode = { 'i', 'n', 's' },
+      },
     },
   },
 
   -- icons
-  { 'nvim-tree/nvim-web-devicons', lazy = true },
+  { 'nvim-tree/nvim-web-devicons' },
 
   -- ui components
-  { 'MunifTanjim/nui.nvim', lazy = true },
+  { 'MunifTanjim/nui.nvim' },
 
   {
     'nvimdev/dashboard-nvim',
@@ -635,13 +864,7 @@ return {
           -- stylua: ignore
           shortcut = {
             {
-              desc = '󰊳 Lazy Update',
-              group = '@property',
-              action = 'Lazy update',
-              key = 'u'
-            },
-            {
-              icon = ' ',
+              icon = ' ',
               icon_hl = '@variable',
               desc = 'Find Files',
               group = 'Label',
@@ -649,10 +872,38 @@ return {
               key = 'f',
             },
             {
-              desc = ' Apps',
-              group = 'DiagnosticHint',
-              action = 'Telescope app',
-              key = 'a',
+              icon = ' ',
+              icon_hl = '@variable',
+              desc = 'Find Text',
+              group = 'Label',
+              action = 'Telescope live_grep',
+              key = 'g',
+            },
+            {
+              icon = ' ',
+              desc = 'New file',
+              group = 'Label',
+              action = 'ene | startinsert',
+              key = 'n',
+            },
+            {
+              icon = ' ',
+              desc = 'Config Files',
+              group = 'Label',
+              action = [[lua require("util").telescope.config_files()()]],
+              key = 'c',
+            },
+            {
+              desc = '󰊳 Lazy',
+              group = '@property',
+              action = 'Lazy',
+              key = 'l'
+            },
+            {
+              desc = '󰊳 Mason',
+              group = '@property',
+              action = 'Mason',
+              key = 'm'
             },
             {
               desc = ' Theme',
