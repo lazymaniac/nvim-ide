69160ee4cb67d5593265e0bed9b263bbb52bb7b6
Sebastian Fabisz <seba@Asus.>
2024-01-12 23:02:21 +0000

    fixed java mapping, instead of opening dir - reveal current buffer in neotree

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -305,6 +305,8 @@ return {
   -- and more.
   {
     'echasnovski/mini.surround',
+    event = 'VeryLazy',
+    version = false,
     keys = function(_, keys)
       -- Populate the keys based on the user's options
       local plugin = require('lazy.core.config').spec.plugins['mini.surround']
--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -25,7 +25,25 @@ return {
       {
         '<leader>e',
         function()
-          require('neo-tree.command').execute { action = 'focus', position = 'float', dir = Util.root() }
+          -- require('neo-tree.command').execute { action = 'focus', position = 'float', dir = Util.root() }
+          local reveal_file = vim.fn.expand '%:p'
+          if reveal_file == '' then
+            reveal_file = vim.fn.getcwd()
+          else
+            local f = io.open(reveal_file, 'r')
+            if f then
+              f.close(f)
+            else
+              reveal_file = vim.fn.getcwd()
+            end
+          end
+          require('neo-tree.command').execute {
+            action = 'focus', -- OPTIONAL, this is the default value
+            source = 'filesystem', -- OPTIONAL, this is the default value
+            position = 'float', -- OPTIONAL, this is the default value
+            reveal_file = reveal_file, -- path to file or folder to reveal
+            reveal_force_cwd = false, -- change cwd without asking if needed
+          }
         end,
         desc = 'Explorer NeoTree (root dir)',
       },
