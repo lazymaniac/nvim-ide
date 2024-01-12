69dee1c42d4f3b864961fb454a4d84bad5883a7d
Sebastian Fabisz <seba@Asus.>
2024-01-12 20:30:45 +0000

    update health check, replace mini.pairs with nvim-pairs

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -272,48 +272,31 @@ return {
 
   -- auto pairs
   {
-    'echasnovski/mini.pairs',
-    event = 'VeryLazy',
-    opts = {
-      -- In which modes mappings from this `config` should be created
-      modes = { insert = true, command = false, terminal = false },
-
-      -- Global mappings. Each right hand side should be a pair information, a
-      -- table with at least these fields (see more in |MiniPairs.map|):
-      -- - <action> - one of 'open', 'close', 'closeopen'.
-      -- - <pair> - two character string for pair to be used.
-      -- By default pair is not inserted after `\`, quotes are not recognized by
-      -- `<CR>`, `'` does not insert pair after a letter.
-      -- Only parts of tables can be tweaked (others will use these defaults).
-      mappings = {
-        ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
-        ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
-        ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\].' },
-
-        [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
-        [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
-        ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
-
-        ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\].', register = { cr = false } },
-        ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\].', register = { cr = false } },
-        ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\].', register = { cr = false } },
-      },
-    },
-    keys = {
-      {
-        '<leader>up',
-        function()
-          local Util = require 'lazy.core.util'
-          vim.g.minipairs_disable = not vim.g.minipairs_disable
-          if vim.g.minipairs_disable then
-            Util.warn('Disabled auto pairs', { title = 'Option' })
-          else
-            Util.info('Enabled auto pairs', { title = 'Option' })
-          end
-        end,
-        desc = 'Toggle auto pairs',
-      },
-    },
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = function()
+      require('nvim-autopairs').setup {
+        disable_filetype = { 'TelescopePrompt', 'vim' },
+        disable_in_macro = true, -- disable when recording or executing a macro
+        disable_in_visualblock = false, -- disable when insert after visual block mode
+        disable_in_replace_mode = true,
+        ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
+        enable_moveright = true,
+        enable_afterquote = true, -- add bracket pairs after quote
+        enable_check_bracket_line = true, --- check bracket in same line
+        enable_bracket_in_quote = true, --
+        enable_abbr = false, -- trigger abbreviation
+        break_undo = true, -- switch for basic rule break undo sequence
+        check_ts = false,
+        map_cr = true,
+        map_bs = true, -- map the <BS> key
+        map_c_h = true, -- Map the <C-h> key to delete a pair
+        map_c_w = true, -- map <c-w> to delete a pair if possible
+      }
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      local cmp = require 'cmp'
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
   },
 
   -- Fast and feature-rich surround actions. For text that includes
