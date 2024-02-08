7191feeb9e044a11c75e2abd3800501555ab7d06
Sebastian Fabisz <seba@Asus.>
2024-02-08 04:09:45 +0000

    gradle workflow task, remove java version change tasks

---
+++ lua/overseer/template/user/gradle-workflow.lua
@@ -0,0 +1,106 @@
+local path = require 'plenary.path'
+
+local function is_gradle_in_cwd()
+  return path:new(vim.fn.getcwd() .. '/gradlew'):exists()
+end
+
+-- Function to run a shell command in the background
+local function run_command_in_background(cmd, callback)
+  local output = {}
+  local job_id
+  local on_stdout = function(_, data)
+    if data then
+      for _, line in ipairs(data) do
+        if line ~= '' then -- Ignore empty strings (last data event)
+          table.insert(output, line)
+        end
+      end
+    end
+  end
+  local on_exit = function(_, code)
+    if code == 0 then
+      callback(table.concat(output, '\n'))
+    else
+      print('Command failed with exit code', code)
+    end
+  end
+  -- Start the job
+  job_id = vim.fn.jobstart(cmd, {
+    on_stdout = on_stdout,
+    on_exit = on_exit,
+    stdout_buffered = true,
+  })
+  if job_id == 0 then
+    print 'Failed to run gradle command: Not executable'
+  elseif job_id == -1 then
+    print 'Failed to run gradle command: Invalid arguments'
+  end
+end
+
+local provider = {
+  condition = {
+    callback = is_gradle_in_cwd,
+  },
+  generator = function(opts, cb)
+    run_command_in_background('./gradlew tasks', function(tasks_output)
+      local ret = {}
+      local gradle_tasks = {}
+
+      require 'notify' 'Loading available gradle tasks'
+
+      for task in tasks_output:gmatch '(%w+)%s%-' do
+        if task and not gradle_tasks[task] then
+          gradle_tasks[task] = true
+        end
+      end
+
+      gradle_tasks = vim.tbl_keys(gradle_tasks)
+      table.sort(gradle_tasks)
+
+      require 'notify' 'Finished loading gradle tasks'
+
+      table.insert(ret, {
+        name = 'Gradle',
+        params = {
+          tasks = {
+            type = 'list',
+            desc = 'Selec task(s)',
+            subtype = {
+              type = 'enum',
+              choices = gradle_tasks,
+            },
+            delimiter = ' ',
+            order = 1,
+          },
+          extra_params = {
+            type = 'string',
+            desc = 'Add extra parameter(s)',
+            optional = true,
+            order = 2,
+          },
+        },
+        builder = function(params)
+          local args = {}
+
+          if params.tasks then
+            for _, task in ipairs(params.tasks) do
+              table.insert(args, task)
+            end
+          end
+
+          return {
+            cmd = './gradlew',
+            args = args,
+          }
+        end,
+        priority = 40,
+        condition = {
+          callback = is_gradle_in_cwd,
+        },
+      })
+      cb(ret)
+    end)
+  end,
+}
+
+return provider
--- lua/overseer/template/user/init.lua
+++ lua/overseer/template/user/init.lua
@@ -1,14 +1,7 @@
 return {
   'user.mvn-workflow',
+  'user.gradle-workflow',
   'user.docker-compose-up',
   'user.docker-compose-kill',
   'user.docker-compose-stop',
-  'user.gradle-build',
-  'user.gradle-test',
-  'user.gradle-tasks',
-  'user.gradle-run',
-  'user.use-java-8',
-  'user.use-java-11',
-  'user.use-java-17',
-  'user.use-java-21',
 }
