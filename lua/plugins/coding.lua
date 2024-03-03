27436d269f48e7d1e1b1e10354aa7a7510abd3ab
lazymaniac <sebastian.fabisz@gmail.com>
2024-03-03 19:53:04 +0000

    UI tweaks for telescope na DevDocs

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -376,10 +376,13 @@ return {
 
   -- [nvim-devdocs] - Dev docs
   -- see: `:h nvim-devdocs`
-  -- TODO: Tweak config
   {
     'luckasRanarison/nvim-devdocs',
     dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim', 'nvim-treesitter/nvim-treesitter' },
+    -- stylua: ignore
+    keys = {
+      { '<leader>ld', '<cmd>DevdocsOpenFloat<cr>', mode = { 'n' }, desc = 'Open DevDocs' },
+    },
     config = function()
       require('nvim-devdocs').setup {
         dir_path = vim.fn.stdpath 'data' .. '/devdocs', -- installation directory
@@ -390,21 +393,20 @@ return {
         },
         float_win = { -- passed to nvim_open_win(), see :h api-floatwin
           relative = 'editor',
-          height = 25,
-          width = 100,
+          height = 35,
+          width = 160,
           border = 'rounded',
         },
         wrap = false, -- text wrap, only applies to floating window
         previewer_cmd = 'glow', -- for example: "glow"
-        cmd_args = { '-s', 'dark', '-w', '80' }, -- example using glow: { "-s", "dark", "-w", "80" }
+        cmd_args = { '-s', 'dark', '-w', '150' }, -- example using glow: { "-s", "dark", "-w", "80" }
         cmd_ignore = {}, -- ignore cmd rendering for the listed docs
-        picker_cmd = false, -- use cmd previewer in picker preview
-        picker_cmd_args = {}, -- example using glow: { "-s", "dark", "-w", "50" }
+        picker_cmd = true, -- use cmd previewer in picker preview
+        picker_cmd_args = { '-s', 'dark', '-w', '100' }, -- example using glow: { "-s", "dark", "-w", "50" }
         mappings = { -- keymaps for the doc buffer
           open_in_browser = '',
         },
         ensure_installed = {}, -- get automatically installed
-        after_open = function(bufnr) end, -- callback that runs after the Devdocs window is opened. Devdocs buffer ID will be passed in
       }
     end,
   },
