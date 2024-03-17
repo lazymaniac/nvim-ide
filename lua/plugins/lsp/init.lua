dd81bf73672ea01d50806316dcc246736e0a4c14
lazymaniac <sebastian.fabisz@gmail.com>
2024-03-17 13:24:15 +0000

    add lsplinks and modes.nvim

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -313,4 +313,13 @@ return {
       },
     },
   },
+
+  {
+    'icholy/lsplinks.nvim',
+    setup = function()
+      local lsplinks = require 'lsplinks'
+      lsplinks.setup()
+      vim.keymap.set('n', 'gx', lsplinks.gx)
+    end,
+  },
 }
