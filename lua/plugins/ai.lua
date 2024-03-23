ffb19b0410f0e93f85ae2e824949f184cc173689
lazymaniac <sebastian.fabisz@gmail.com>
2024-03-23 13:43:13 +0000

    testing codecompanion as replacement for ChatGPT. Switched persistence.nvim to persisted.nvim

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -2,10 +2,97 @@ return {
 
   -- [[ AI ]] ---------------------------------------------------------------
 
+  {
+    'folke/edgy.nvim',
+    event = 'VeryLazy',
+    enabled = true,
+    init = function()
+      vim.opt.laststatus = 3
+      vim.opt.splitkeep = 'screen'
+    end,
+    opts = {
+      right = {
+        { ft = 'codecompanion', title = 'Code Companion Chat', size = { width = 0.40 } },
+      },
+    },
+  },
+
+  {
+    'olimorris/codecompanion.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter', 'nvim-telescope/telescope.nvim' },
+    -- stylua: ignore
+    keys = {
+      { '<leader>zg', '<cmd>CodeCompanion<cr>', mode = { 'n', 'v' }, desc = 'Code Companion [zg]' },
+      { '<leader>zc', '<cmd>CodeCompanionChat<cr>', mode = { 'n', 'v' }, desc = 'Chat [zc]' },
+      { '<leader>zt', '<cmd>CodeCompanionToggle<cr>', mode = { 'n', 'v' }, desc = 'Toggle [zt]' },
+      { '<leader>za', '<cmd>CodeCompanionActions<cr>', mode = { 'n', 'v' }, desc = 'Actions [za]' },
+    },
+    config = function()
+      require('codecompanion').setup {
+        adapters = { -- anthropic|ollama|openai
+          chat = require('codecompanion.adapters').use('openai', {
+            env = {
+              api_key = 'cmd:cat ' .. vim.fn.expand '~/.gpt',
+            },
+          }),
+          inline = 'openai',
+        },
+        saved_chats = {
+          save_dir = vim.fn.stdpath 'data' .. '/codecompanion/saved_chats', -- Path to save chats to
+        },
+        display = {
+          action_palette = {
+            width = 95,
+            height = 10,
+          },
+          chat = { -- Options for the chat strategy
+            type = 'float', -- float|buffer
+            show_settings = true, -- Show the model settings in the chat buffer?
+            show_token_count = true, -- Show the token count for the current chat in the buffer?
+            buf_options = { -- Buffer options for the chat buffer
+              buflisted = false,
+            },
+            float_options = { -- Float window options if the type is "float"
+              border = 'single',
+              buflisted = false,
+              max_height = 0,
+              max_width = 0,
+              padding = 1,
+            },
+            win_options = { -- Window options for the chat buffer
+              cursorcolumn = false,
+              cursorline = false,
+              foldcolumn = '0',
+              linebreak = true,
+              list = false,
+              signcolumn = 'no',
+              spell = false,
+              wrap = true,
+            },
+          },
+        },
+        keymaps = {
+          ['<C-s>'] = 'keymaps.save', -- Save the chat buffer and trigger the API
+          ['<C-c>'] = 'keymaps.close', -- Close the chat buffer
+          ['q'] = 'keymaps.cancel_request', -- Cancel the currently streaming request
+          ['gc'] = 'keymaps.clear', -- Clear the contents of the chat
+          ['ga'] = 'keymaps.codeblock', -- Insert a codeblock into the chat
+          ['gs'] = 'keymaps.save_chat', -- Save the current chat
+          [']'] = 'keymaps.next', -- Move to the next header in the chat
+          ['['] = 'keymaps.previous', -- Move to the previous header in the chat
+        },
+        log_level = 'ERROR', -- TRACE|DEBUG|ERROR
+        send_code = true, -- Send code context to the generative AI service? Disable to prevent leaking code outside of Neovim
+        silence_notifications = false, -- Silence notifications for actions like saving saving chats?
+        use_default_actions = true, -- Use the default actions in the action palette?
+      }
+    end,
+  },
   -- [ChatGPT.nvim] - Integration with ChatGPT.
   -- see: `:h ChatGPT.nvim`
   {
     'jackMort/ChatGPT.nvim',
+    enabled = false,
     event = 'VeryLazy',
     dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
     config = function()
@@ -187,6 +274,7 @@ return {
   -- see: `:h gen.nvim`
   {
     'David-Kunz/gen.nvim',
+    enabled = true,
     event = 'VeryLazy',
     config = function()
       require('gen').setup {
