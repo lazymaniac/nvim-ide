cd64e41b188aecdc8e504f933b5f9113fbbae2d7
lazymaniac <sebastian.fabisz@gmail.com>
2024-03-24 15:34:48 +0000

    code cleanup

--- lua/plugins/images.lua
+++ lua/plugins/images.lua
@@ -49,24 +49,4 @@ return {
     },
   },
 
-  {
-    'HakonHarnes/img-clip.nvim',
-    event = 'BufEnter',
-    opts = {
-      markdown = {
-        url_encode_path = true,
-        template = '![$CURSOR]($FILE_PATH)',
-        drag_and_drop = {
-          download_images = false,
-        },
-      },
-      quarto = {
-        url_encode_path = true,
-        template = '![$CURSOR]($FILE_PATH)',
-        drag_and_drop = {
-          download_images = false,
-        },
-      },
-    },
-  },
 }
