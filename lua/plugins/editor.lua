3eaaae7e4b139af6e23ec5575e4e77ca65a2063b
lazymaniac <sebastian.fabisz@gmail.com>
2024-03-24 15:34:48 +0000

    code cleanup

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -722,6 +722,8 @@ return {
     end,
   },
 
+  -- [nvim-scrollbar] - Scorllbar with git and lsp integration
+  -- see: `:h nvim-scrollbar`
   {
     'petertriho/nvim-scrollbar',
     config = function()
@@ -781,6 +783,8 @@ return {
     end,
   },
 
+  -- [bigfile.nvim] - Disable editor functions like lsp, treesitter when big file is loaded
+  -- see: `:h bigfile.nvim`
   {
     'LunarVim/bigfile.nvim',
     init = function()
@@ -803,17 +807,21 @@ return {
 
   { 'tpope/vim-repeat' },
 
+  -- [gx.nvim] - Open link in browser
+  -- see: `:h gx.nvim`
   {
     'chrishrb/gx.nvim',
     event = { 'BufEnter' },
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = {
       handler_options = {
-        search_engine = 'duckduckgo',
+        search_engine = 'google',
       },
     },
   },
 
+  -- [projections.nvim] - Plugin to manage project in defined directory (workspace)
+  -- see: `:h projections.nvim`
   {
     'gnikdroy/projections.nvim',
     -- stylua: ignore
@@ -855,6 +863,8 @@ return {
     end,
   },
 
+  -- [modes.nvim] - Highlights current line accordingly to mode
+  -- see: `:h modes.nvim`
   {
     'mvllow/modes.nvim',
     tag = 'v0.2.0',
@@ -882,6 +892,8 @@ return {
     end,
   },
 
+  -- [wilder.nvim] - cmdline improving plugin
+  -- see: `:h wilder.nvim`
   {
     'gelguy/wilder.nvim',
     config = function()
@@ -908,17 +920,41 @@ return {
           }
         ),
       })
+      local gradient = {
+        '#f4468f',
+        '#fd4a85',
+        '#ff507a',
+        '#ff566f',
+        '#ff5e63',
+        '#ff6658',
+        '#ff704e',
+        '#ff7a45',
+        '#ff843d',
+        '#ff9036',
+        '#f89b31',
+        '#efa72f',
+        '#e6b32e',
+        '#dcbe30',
+        '#d2c934',
+        '#c8d43a',
+        '#bfde43',
+        '#b6e84e',
+        '#aff05b',
+      }
+      for i, fg in ipairs(gradient) do
+        gradient[i] = wilder.make_hl('WilderGradient' .. i, 'Pmenu', { { a = 1 }, { a = 1 }, { foreground = fg } })
+      end
       wilder.set_option(
         'renderer',
-        wilder.popupmenu_renderer(wilder.popupmenu_palette_theme {
-          -- 'single', 'double', 'rounded' or 'solid'
-          -- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
-          border = 'rounded',
-          max_height = '75%', -- max height of the palette
-          min_height = 0, -- set to the same as 'max_height' for a fixed height window
-          prompt_position = 'top', -- 'top' or 'bottom' to set the location of the prompt
-          reverse = 0, -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
-        })
+        wilder.popupmenu_renderer {
+          highlights = {
+            gradient = gradient, -- must be set
+            -- selected_gradient key can be set to apply gradient highlighting for the selected candidate.
+          },
+          highlighter = wilder.highlighter_with_gradient {
+            wilder.basic_highlighter(), -- or wilder.lua_fzy_highlighter(),
+          },
+        }
       )
     end,
   },
