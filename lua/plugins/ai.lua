da0d6d32f138df27f99502fdd3230c005257a1e4
lazymaniac <sebastian.fabisz@gmail.com>
2024-03-31 03:13:10 +0100

    small tweaks

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,6 +1,6 @@
-local ollama_response_format = 'Respond EXACTLY in this format:\n```$ftype\n<your code>\n```'
+local response_format = 'Respond EXACTLY in this format:\n```$ftype\n<your code>\n```'
 
-local ollama_general_model = 'gemma:7b-instruct-q6_K'
+local ollama_general_model = 'gemma'
 -- local ollama_coder_model = 'dolphincoder'
 local ollama_coder_model = 'opencoder'
 
@@ -42,10 +42,10 @@ return {
     },
     cmd = { 'Ollama', 'OllamaModel', 'OllamaServe', 'OllamaServeStop' },
     keys = {
-      { '<leader>zz', ":<c-u>lua require('ollama').prompt()<cr>", desc = 'ollama prompt', mode = { 'n', 'v' } },
+      { '<leader>zz', ":<c-u>lua require('ollama').prompt()<cr>", desc = 'Local LLMs [zz]', mode = { 'n', 'v' } },
     },
     opts = {
-      model = 'gemma:7b-instruct-q6_K',
+      model = 'gemma',
       url = 'http://127.0.0.1:11434',
       serve = {},
       prompts = {
@@ -105,7 +105,7 @@ return {
         Simplify_Code = {
           model = ollama_coder_model,
           prompt = 'Simplify the following $ftype code so that it is both easier to read and understand. '
-            .. ollama_response_format
+            .. response_format
             .. '\n\n```$ftype\n$sel```',
           system = aideveloper_prompt_ollama,
           action = 'display_replace',
@@ -117,7 +117,7 @@ return {
         },
         Modify_Code = {
           model = ollama_coder_model,
-          prompt = 'Modify this $ftype code in the following way: $input\n\n' .. ollama_response_format .. '\n\n```$ftype\n$sel```',
+          prompt = 'Modify this $ftype code in the following way: $input\n\n' .. response_format .. '\n\n```$ftype\n$sel```',
           system = aideveloper_prompt_ollama,
           action = 'display_replace',
           extract = '```$ftype\n(.-)```',
@@ -129,7 +129,7 @@ return {
         },
         Implement_Code = {
           model = ollama_coder_model,
-          prompt = 'Implement code according to instructions: $input.' .. ollama_response_format,
+          prompt = 'Implement code according to instructions: $input.' .. response_format,
           system = aideveloper_prompt_ollama,
           action = 'display_insert',
           extract = '```$ftype\n(.-)```',
@@ -141,7 +141,7 @@ return {
         },
         Implement_Tests = {
           model = ollama_coder_model,
-          prompt = 'Implement tests for provided $ftype code: \n$sel.\n\n' .. ollama_response_format,
+          prompt = 'Implement tests for provided $ftype code: \n$sel.\n\n' .. response_format,
           system = aideveloper_prompt_ollama,
           action = 'display_insert',
           extract = '```$ftype\n(.-)```',
@@ -193,7 +193,7 @@ return {
 
   {
     'olimorris/codecompanion.nvim',
-    enabled = false,
+    enabled = true,
     dependencies = { 'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter', 'nvim-telescope/telescope.nvim' },
     -- stylua: ignore
     keys = {
@@ -349,272 +349,4 @@ return {
     end,
   },
 
-  {
-    'robitx/gp.nvim',
-    config = function()
-      require('gp').setup {
-        openai_api_key = { 'cat', vim.fn.expand '~/.gpt' },
-        -- api endpoint (you can change this to azure endpoint)
-        openai_api_endpoint = 'https://api.openai.com/v1/chat/completions',
-        -- openai_api_endpoint = "https://$URL.openai.azure.com/openai/deployments/{{model}}/chat/completions?api-version=2023-03-15-preview",
-        -- prefix for all commands
-        cmd_prefix = 'Gp',
-        -- directory for persisting state dynamically changed by user (like model or persona)
-        state_dir = vim.fn.stdpath('data'):gsub('/$', '') .. '/gp/persisted',
-        -- default command agents (model + persona)
-        -- name, model and system_prompt are mandatory fields
-        -- to use agent for chat set chat = true, for command set command = true
-        -- to remove some default agent completely set it just with the name like:
-        -- agents = {  { name = "ChatGPT4" }, ... },
-        agents = {
-          {
-            name = 'ChatGPT4',
-            chat = true,
-            command = false,
-            -- string with model name or table with model name and parameters
-            model = { model = 'gpt-4-turbo-preview', temperature = 1.1, top_p = 1 },
-            -- system prompt (use this to specify the persona/role of the AI)
-            system_prompt = aideveloper_prompt_gpt,
-          },
-          {
-            name = 'ChatGPT3-5',
-          },
-          {
-            name = 'CodeGPT4',
-            chat = false,
-            command = true,
-            -- string with model name or table with model name and parameters
-            model = { model = 'gpt-4-turbo-preview', temperature = 0.8, top_p = 1 },
-            -- system prompt (use this to specify the persona/role of the AI)
-            system_prompt = aideveloper_prompt_gpt .. 'Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n' .. 'START AND END YOUR ANSWER WITH:\n\n```',
-          },
-          {
-            name = 'CodeGPT3-5',
-          },
-        },
-
-        -- directory for storing chat files
-        chat_dir = vim.fn.stdpath('data'):gsub('/$', '') .. '/gp/chats',
-        -- chat user prompt prefix
-        chat_user_prefix = '󰺴 :',
-        -- chat assistant prompt prefix (static string or a table {static, template})
-        -- first string has to be static, second string can contain template {{agent}}
-        -- just a static string is legacy and the [{{agent}}] element is added automatically
-        -- if you really want just a static string, make it a table with one element { "🤖:" }
-        chat_assistant_prefix = { '󰁤 :', '[{{agent}}]' },
-        -- chat topic generation prompt
-        chat_topic_gen_prompt = 'Summarize the topic of our conversation above' .. ' in two or three words. Respond only with those words.',
-        -- chat topic model (string with model name or table with model name and parameters)
-        chat_topic_gen_model = 'gpt-4-turbo-preview',
-        -- explicitly confirm deletion of a chat file
-        chat_confirm_delete = true,
-        -- conceal model parameters in chat
-        chat_conceal_model_params = true,
-        -- local shortcuts bound to the chat buffer
-        -- (be careful to choose something which will work across specified modes)
-        chat_shortcut_respond = { modes = { 'n', 'i', 'v', 'x' }, shortcut = '<C-g><C-g>' },
-        chat_shortcut_delete = { modes = { 'n', 'i', 'v', 'x' }, shortcut = '<C-g>d' },
-        chat_shortcut_stop = { modes = { 'n', 'i', 'v', 'x' }, shortcut = '<C-g>s' },
-        chat_shortcut_new = { modes = { 'n', 'i', 'v', 'x' }, shortcut = '<C-g>c' },
-        -- default search term when using :GpChatFinder
-        chat_finder_pattern = 'topic ',
-        -- if true, finished ChatResponder won't move the cursor to the end of the buffer
-        chat_free_cursor = false,
-
-        -- how to display GpChatToggle or GpContext: popup / split / vsplit / tabnew
-        toggle_target = 'vsplit',
-
-        -- styling for chatfinder
-        -- border can be "single", "double", "rounded", "solid", "shadow", "none"
-        style_chat_finder_border = 'rounded',
-        -- margins are number of characters or lines
-        style_chat_finder_margin_bottom = 8,
-        style_chat_finder_margin_left = 1,
-        style_chat_finder_margin_right = 2,
-        style_chat_finder_margin_top = 2,
-        -- how wide should the preview be, number between 0.0 and 1.0
-        style_chat_finder_preview_ratio = 0.4,
-
-        -- styling for popup
-        -- border can be "single", "double", "rounded", "solid", "shadow", "none"
-        style_popup_border = 'rounded',
-        -- margins are number of characters or lines
-        style_popup_margin_bottom = 8,
-        style_popup_margin_left = 1,
-        style_popup_margin_right = 2,
-        style_popup_margin_top = 2,
-        style_popup_max_width = 160,
-
-        -- command config and templates bellow are used by commands like GpRewrite, GpEnew, etc.
-        -- command prompt prefix for asking user for input (supports {{agent}} template variable)
-        command_prompt_prefix_template = '󰁤 {{agent}} ~ ',
-        -- auto select command response (easier chaining of commands)
-        -- if false it also frees up the buffer cursor for further editing elsewhere
-        command_auto_select_response = true,
-
-        -- templates
-        template_selection = 'I have the following from {{filename}}:' .. '\n\n```{{filetype}}\n{{selection}}\n```\n\n{{command}}',
-        template_rewrite = 'I have the following from {{filename}}:'
-          .. '\n\n```{{filetype}}\n{{selection}}\n```\n\n{{command}}'
-          .. '\n\nRespond exclusively with the snippet that should replace the selection above.',
-        template_append = 'I have the following from {{filename}}:'
-          .. '\n\n```{{filetype}}\n{{selection}}\n```\n\n{{command}}'
-          .. '\n\nRespond exclusively with the snippet that should be appended after the selection above.',
-        template_prepend = 'I have the following from {{filename}}:'
-          .. '\n\n```{{filetype}}\n{{selection}}\n```\n\n{{command}}'
-          .. '\n\nRespond exclusively with the snippet that should be prepended before the selection above.',
-        template_command = '{{command}}',
-
-        -- https://platform.openai.com/docs/guides/speech-to-text/quickstart
-        -- Whisper costs $0.006 / minute (rounded to the nearest second)
-        -- by eliminating silence and speeding up the tempo of the recording
-        -- we can reduce the cost by 50% or more and get the results faster
-        -- directory for storing whisper files
-        whisper_dir = (os.getenv 'TMPDIR' or os.getenv 'TEMP' or '/tmp') .. '/gp_whisper',
-        -- multiplier of RMS level dB for threshold used by sox to detect silence vs speech
-        -- decibels are negative, the recording is normalized to -3dB =>
-        -- increase this number to pick up more (weaker) sounds as possible speech
-        -- decrease this number to pick up only louder sounds as possible speech
-        -- you can disable silence trimming by setting this a very high number (like 1000.0)
-        whisper_silence = '1.75',
-        -- whisper tempo (1.0 is normal speed)
-        whisper_tempo = '1.00',
-        -- The language of the input audio, in ISO-639-1 format.
-        whisper_language = 'en',
-
-        -- example hook functions (see Extend functionality section in the README)
-        hooks = {
-          InspectPlugin = function(plugin, params)
-            local bufnr = vim.api.nvim_create_buf(false, true)
-            local copy = vim.deepcopy(plugin)
-            local key = copy.config.openai_api_key
-            copy.config.openai_api_key = key:sub(1, 3) .. string.rep('*', #key - 6) .. key:sub(-3)
-            local plugin_info = string.format('Plugin structure:\n%s', vim.inspect(copy))
-            local params_info = string.format('Command params:\n%s', vim.inspect(params))
-            local lines = vim.split(plugin_info .. '\n' .. params_info, '\n')
-            vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
-            vim.api.nvim_win_set_buf(0, bufnr)
-          end,
-
-          -- GpImplement rewrites the provided selection/range based on comments in it
-          Implement = function(gp, params)
-            local template = 'Having following from {{filename}}:\n\n'
-              .. '```{{filetype}}\n{{selection}}\n```\n\n'
-              .. 'Please rewrite this according to the contained instructions.'
-              .. '\n\nRespond exclusively with the snippet that should replace the selection above.'
-
-            local agent = gp.get_command_agent()
-            gp.info('Implementing selection with agent: ' .. agent.name)
-
-            gp.Prompt(
-              params,
-              gp.Target.rewrite,
-              nil, -- command will run directly without any prompting for user input
-              agent.model,
-              template,
-              agent.system_prompt
-            )
-          end,
-
-          -- your own functions can go here, see README for more examples like
-          -- :GpExplain, :GpUnitTests.., :GpTranslator etc.
-
-          -- example of making :%GpChatNew a dedicated command which
-          -- opens new chat with the entire current buffer as a context
-          BufferChatNew = function(gp, _)
-            -- call GpChatNew command in range mode on whole buffer
-            vim.api.nvim_command('%' .. gp.config.cmd_prefix .. 'ChatNew')
-          end,
-
-          -- example of adding command which opens new chat dedicated for translation
-          Translator = function(gp, params)
-            local agent = gp.get_command_agent()
-            local chat_system_prompt = 'You are a Translator, please translate between Polish and English.'
-            gp.cmd.ChatNew(params, agent.model, chat_system_prompt)
-          end,
-
-          -- example of adding command which writes unit tests for the selected code
-          UnitTests = function(gp, params)
-            local template = 'I have the following code from {{filename}}:\n\n'
-              .. '```{{filetype}}\n{{selection}}\n```\n\n'
-              .. 'Please respond by writing table driven unit tests for the code above.'
-            local agent = gp.get_command_agent()
-            gp.Prompt(params, gp.Target.enew, nil, agent.model, template, agent.system_prompt)
-          end,
-
-          -- example of adding command which explains the selected code
-          Explain = function(gp, params)
-            local template = 'I have the following code from {{filename}}:\n\n'
-              .. '```{{filetype}}\n{{selection}}\n```\n\n'
-              .. 'Please respond by explaining the code above.'
-            local agent = gp.get_chat_agent()
-            gp.Prompt(params, gp.Target.popup, nil, agent.model, template, agent.system_prompt)
-          end,
-        },
-      }
-      local function keymapOptions(desc)
-        return {
-          noremap = true,
-          silent = true,
-          nowait = true,
-          desc = 'GPT prompt ' .. desc,
-        }
-      end
-
-      -- Chat commands
-      vim.keymap.set({ 'n', 'i' }, '<C-g>c', '<cmd>GpChatNew<cr>', keymapOptions 'New Chat')
-      vim.keymap.set({ 'n', 'i' }, '<C-g>t', '<cmd>GpChatToggle<cr>', keymapOptions 'Toggle Chat')
-      vim.keymap.set({ 'n', 'i' }, '<C-g>f', '<cmd>GpChatFinder<cr>', keymapOptions 'Chat Finder')
-
-      vim.keymap.set('v', '<C-g>c', ":<C-u>'<,'>GpChatNew<cr>", keymapOptions 'Chat New')
-      vim.keymap.set('v', '<C-g>p', ":<C-u>'<,'>GpChatPaste<cr>", keymapOptions 'Chat Paste')
-      vim.keymap.set('v', '<C-g>t', ":<C-u>'<,'>GpChatToggle<cr>", keymapOptions 'Toggle Chat')
-
-      vim.keymap.set({ 'n', 'i' }, '<C-g><C-v>', '<cmd>GpChatNew vsplit<cr>', keymapOptions 'New Chat vsplit')
-
-      vim.keymap.set('v', '<C-g><C-v>', ":<C-u>'<,'>GpChatNew vsplit<cr>", keymapOptions 'Chat New vsplit')
-
-      -- Prompt commands
-      vim.keymap.set({ 'n', 'i' }, '<C-g>r', '<cmd>GpRewrite<cr>', keymapOptions 'Inline Rewrite')
-      vim.keymap.set({ 'n', 'i' }, '<C-g>a', '<cmd>GpAppend<cr>', keymapOptions 'Append (after)')
-      vim.keymap.set({ 'n', 'i' }, '<C-g>b', '<cmd>GpPrepend<cr>', keymapOptions 'Prepend (before)')
-
-      vim.keymap.set('v', '<C-g>r', ":<C-u>'<,'>GpRewrite<cr>", keymapOptions 'Rewrite')
-      vim.keymap.set('v', '<C-g>a', ":<C-u>'<,'>GpAppend<cr>", keymapOptions 'Append (after)')
-      vim.keymap.set('v', '<C-g>b', ":<C-u>'<,'>GpPrepend<cr>", keymapOptions 'Visual Prepend (before)')
-      vim.keymap.set('v', '<C-g>i', ":<C-u>'<,'>GpImplement<cr>", keymapOptions 'Implement selection')
-      vim.keymap.set('v', '<C-g>u', ":<C-u>'<,'>GpUnitTests<cr>", keymapOptions 'Implement Unit Tests')
-      vim.keymap.set('v', '<C-g>u', ":<C-u>'<,'>GpExplain<cr>", keymapOptions 'Explain Selection')
-
-      vim.keymap.set({ 'n', 'i' }, '<C-g>gp', '<cmd>GpPopup<cr>', keymapOptions 'Popup')
-      vim.keymap.set({ 'n', 'i' }, '<C-g>gv', '<cmd>GpVnew<cr>', keymapOptions 'GpVnew')
-
-      vim.keymap.set('v', '<C-g>gp', ":<C-u>'<,'>GpPopup<cr>", keymapOptions 'Popup')
-      vim.keymap.set('v', '<C-g>gv', ":<C-u>'<,'>GpVnew<cr>", keymapOptions 'GpVnew')
-
-      vim.keymap.set({ 'n', 'i' }, '<C-g>x', '<cmd>GpContext<cr>', keymapOptions 'Toggle Context')
-      vim.keymap.set('v', '<C-g>x', ":<C-u>'<,'>GpContext<cr>", keymapOptions 'Toggle Context')
-
-      vim.keymap.set({ 'n', 'i', 'v', 'x' }, '<C-g>s', '<cmd>GpStop<cr>', keymapOptions 'Stop')
-      vim.keymap.set({ 'n', 'i', 'v', 'x' }, '<C-g>n', '<cmd>GpNextAgent<cr>', keymapOptions 'Next Agent')
-
-      -- optional Whisper commands with prefix <C-g>w
-      vim.keymap.set({ 'n', 'i' }, '<C-g>ww', '<cmd>GpWhisper<cr>', keymapOptions 'Whisper')
-      vim.keymap.set('v', '<C-g>ww', ":<C-u>'<,'>GpWhisper<cr>", keymapOptions 'Whisper')
-
-      vim.keymap.set({ 'n', 'i' }, '<C-g>wr', '<cmd>GpWhisperRewrite<cr>', keymapOptions 'Whisper Inline Rewrite')
-      vim.keymap.set({ 'n', 'i' }, '<C-g>wa', '<cmd>GpWhisperAppend<cr>', keymapOptions 'Whisper Append (after)')
-      vim.keymap.set({ 'n', 'i' }, '<C-g>wb', '<cmd>GpWhisperPrepend<cr>', keymapOptions 'Whisper Prepend (before) ')
-
-      vim.keymap.set('v', '<C-g>wr', ":<C-u>'<,'>GpWhisperRewrite<cr>", keymapOptions 'Whisper Rewrite')
-      vim.keymap.set('v', '<C-g>wa', ":<C-u>'<,'>GpWhisperAppend<cr>", keymapOptions 'Whisper Append (after)')
-      vim.keymap.set('v', '<C-g>wb', ":<C-u>'<,'>GpWhisperPrepend<cr>", keymapOptions 'Whisper Prepend (before)')
-
-      vim.keymap.set({ 'n', 'i' }, '<C-g>wp', '<cmd>GpWhisperPopup<cr>', keymapOptions 'Whisper Popup')
-      vim.keymap.set({ 'n', 'i' }, '<C-g>wv', '<cmd>GpWhisperVnew<cr>', keymapOptions 'Whisper Vnew')
-
-      vim.keymap.set('v', '<C-g>wp', ":<C-u>'<,'>GpWhisperPopup<cr>", keymapOptions 'Whisper Popup')
-      vim.keymap.set('v', '<C-g>wv', ":<C-u>'<,'>GpWhisperVnew<cr>", keymapOptions 'Whisper Vnew')
-    end,
-  },
 }
