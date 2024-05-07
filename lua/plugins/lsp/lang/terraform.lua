98ca8b75199af1af4ab29901513ffdfedef344e2
lazymaniac <sebastian.fabisz@gmail.com>
2024-05-07 01:17:41 +0100

    fixes

--- lua/plugins/lsp/lang/terraform.lua
+++ lua/plugins/lsp/lang/terraform.lua
@@ -10,7 +10,7 @@ return {
     'williamboman/mason.nvim',
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { 'tfsec', 'trivy', 'terraform_validate' })
+      vim.list_extend(opts.ensure_installed, { 'tfsec', 'trivy' })
     end,
   },
 
