2fb5857a76e7a1cefb037910752c910eb134c8f8
lazymaniac <sebastian.fabisz@gmail.com>
2024-05-25 18:02:31 +0100

    add persistent-breakpoints plugin

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -20,31 +20,31 @@ return {
     branch = 'master',
     event = 'VeryLazy',
     dependencies = {
-      { 'rcarriga/nvim-dap-ui', branch = 'master' },
+      { 'rcarriga/nvim-dap-ui',            branch = 'master' },
       { 'theHamsta/nvim-dap-virtual-text', branch = 'master' },
-      { 'jay-babu/mason-nvim-dap.nvim', branch = 'main' },
-      { 'stevearc/overseer.nvim', branch = 'master' },
+      { 'jay-babu/mason-nvim-dap.nvim',    branch = 'main' },
+      { 'stevearc/overseer.nvim',          branch = 'master' },
       { 'LiadOz/nvim-dap-repl-highlights', branch = 'master' },
     },
     -- stylua: ignore
     keys = {
       { '<leader>dB', function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end, desc = 'Breakpoint Condition [dB]' },
-      { '<leader>db', function() require('dap').toggle_breakpoint() end, desc = 'Toggle Breakpoint [db]' },
-      { '<leader>dc', function() require('dap').continue() end, desc = 'Continue [dc]' },
-      { '<leader>da', function() require('dap').continue { before = get_args } end, desc = 'Run with Args [da]' },
-      { '<leader>dC', function() require('dap').run_to_cursor() end, desc = 'Run to Cursor [dC]' },
-      { '<leader>dg', function() require('dap').goto_() end, desc = 'Go to line (no execute) [dg]' },
-      { '<leader>di', function() require('dap').step_into() end, desc = 'Step Into [di]' },
-      { '<leader>dj', function() require('dap').down() end, desc = 'Down [dj]' },
-      { '<leader>dk', function() require('dap').up() end, desc = 'Up [dk]' },
-      { '<leader>dl', function() require('dap').run_last() end, desc = 'Run Last [dl]' },
-      { '<leader>dO', function() require('dap').step_out() end, desc = 'Step Out [dO]' },
-      { '<leader>do', function() require('dap').step_over() end, desc = 'Step Over [do]' },
-      { '<leader>dp', function() require('dap').pause() end, desc = 'Pause [dp]' },
-      { '<leader>dr', function() require('dap').repl.toggle() end, desc = 'Toggle REPL [dr]' },
-      { '<leader>ds', function() require('dap').session() end, desc = 'Session [ds]' },
-      { '<leader>dt', function() require('dap').terminate() end, desc = 'Terminate [dt]' },
-      { '<leader>dw', function() require('dap.ui.widgets').hover() end, desc = 'Widgets [dw]' },
+      { '<leader>db', function() require('dap').toggle_breakpoint() end,                                   desc = 'Toggle Breakpoint [db]' },
+      { '<leader>dc', function() require('dap').continue() end,                                            desc = 'Continue [dc]' },
+      { '<leader>da', function() require('dap').continue { before = get_args } end,                        desc = 'Run with Args [da]' },
+      { '<leader>dC', function() require('dap').run_to_cursor() end,                                       desc = 'Run to Cursor [dC]' },
+      { '<leader>dg', function() require('dap').goto_() end,                                               desc = 'Go to line (no execute) [dg]' },
+      { '<leader>di', function() require('dap').step_into() end,                                           desc = 'Step Into [di]' },
+      { '<leader>dj', function() require('dap').down() end,                                                desc = 'Down [dj]' },
+      { '<leader>dk', function() require('dap').up() end,                                                  desc = 'Up [dk]' },
+      { '<leader>dl', function() require('dap').run_last() end,                                            desc = 'Run Last [dl]' },
+      { '<leader>dO', function() require('dap').step_out() end,                                            desc = 'Step Out [dO]' },
+      { '<leader>do', function() require('dap').step_over() end,                                           desc = 'Step Over [do]' },
+      { '<leader>dp', function() require('dap').pause() end,                                               desc = 'Pause [dp]' },
+      { '<leader>dr', function() require('dap').repl.toggle() end,                                         desc = 'Toggle REPL [dr]' },
+      { '<leader>ds', function() require('dap').session() end,                                             desc = 'Session [ds]' },
+      { '<leader>dt', function() require('dap').terminate() end,                                           desc = 'Terminate [dt]' },
+      { '<leader>dw', function() require('dap.ui.widgets').hover() end,                                    desc = 'Widgets [dw]' },
     },
     config = function()
       local dap = require 'dap'
@@ -61,7 +61,8 @@ return {
       vim.api.nvim_set_hl(0, 'DapStoppedLine', { default = true, link = 'Visual' })
       for name, sign in pairs(Config.icons.dap) do
         sign = type(sign) == 'table' and sign or { sign }
-        vim.fn.sign_define('Dap' .. name, { text = sign[1], texthl = sign[2] or 'DiagnosticInfo', linehl = sign[3], numhl = sign[3] })
+        vim.fn.sign_define('Dap' .. name,
+          { text = sign[1], texthl = sign[2] or 'DiagnosticInfo', linehl = sign[3], numhl = sign[3] })
       end
     end,
   },
@@ -78,8 +79,15 @@ return {
     keys = {
       { '<leader>du', function() require('dapui').toggle { layout = 1, reset = true } end, desc = 'Dap Tools [du]' },
       { '<leader>dU', function() require('dapui').toggle { layout = 2, reset = true } end, desc = 'Dap Console [du]' },
-      { '<leader>de', function() require('dapui').eval() end, desc = 'Eval [de]', mode = { 'n', 'v' } },
-      { '<leader>df', function() require('dapui').float_element(nil, { width = 184, height = 44, enter = true, position = 'center' }) end, desc = 'Open floating DAP [df]' },
+      { '<leader>de', function() require('dapui').eval() end,                              desc = 'Eval [de]',       mode = { 'n', 'v' } },
+      {
+        '<leader>df',
+        function()
+          require('dapui').float_element(nil,
+            { width = 184, height = 44, enter = true, position = 'center' })
+        end,
+        desc = 'Open floating DAP [df]'
+      },
     },
     config = function()
       -- setup dap config by VsCode launch.json file
@@ -119,11 +127,11 @@ return {
         layouts = {
           {
             elements = {
-              { id = 'scopes', size = 0.2 },
-              { id = 'stacks', size = 0.2 },
-              { id = 'watches', size = 0.2 },
+              { id = 'scopes',      size = 0.2 },
+              { id = 'stacks',      size = 0.2 },
+              { id = 'watches',     size = 0.2 },
               { id = 'breakpoints', size = 0.1 },
-              { id = 'repl', size = 0.3 },
+              { id = 'repl',        size = 0.3 },
             },
             position = 'right',
             size = 60,
@@ -167,15 +175,15 @@ return {
       { '<leader>dv', '<cmd>DapVirtualTextToggle<cr>', desc = 'Toggle DAP Virtual Text [dv]', mode = { 'n', 'v' } },
     },
     opts = {
-      enabled = true, -- enable this plugin (the default)
-      enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+      enabled = true,                     -- enable this plugin (the default)
+      enabled_commands = true,            -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
       highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-      highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-      show_stop_reason = true, -- show stop reason when stopped for exceptions
-      commented = false, -- prefix virtual text with comment string
-      only_first_definition = false, -- only show virtual text at first definition (if there are multiple)
-      all_references = true, -- show virtual text on all all references of the variable (not only definitions)
-      clear_on_continue = false, -- clear virtual text on "continue" (might cause flickering when stepping)
+      highlight_new_as_changed = false,   -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+      show_stop_reason = true,            -- show stop reason when stopped for exceptions
+      commented = false,                  -- prefix virtual text with comment string
+      only_first_definition = false,      -- only show virtual text at first definition (if there are multiple)
+      all_references = true,              -- show virtual text on all all references of the variable (not only definitions)
+      clear_on_continue = false,          -- clear virtual text on "continue" (might cause flickering when stepping)
       ---@diagnostic disable-next-line: unused-local
       display_callback = function(variable, buf, stackframe, node, options)
         if options.virt_text_pos == 'inline' then
@@ -187,8 +195,8 @@ return {
       -- position of virtual text, see `:h nvim_buf_set_extmark()`, default tries to inline the virtual text. Use 'eol' to set to end of line
       virt_text_pos = vim.fn.has 'nvim-0.10' == 1 and 'inline' or 'eol',
       -- experimental features:
-      all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-      virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
+      all_frames = false,      -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+      virt_lines = false,      -- show virtual lines instead of virtual text (will flicker!)
       virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
       -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
     },
@@ -230,4 +238,18 @@ return {
       require('nvim-dap-repl-highlights').setup()
     end,
   },
+
+  -- [persistent-breakpoints.nvim] - Store breakpoints location on disk and load them on buffer open event.
+  -- see: `:h persistent-breakpoints.nvim`
+  -- link: https://github.com/Weissle/persistent-breakpoints.nvim
+  {
+    'Weissle/persistent-breakpoints.nvim',
+    branch = 'main',
+    opts = {
+      save_dir = vim.fn.stdpath('cache') .. '/nvim_breakpoints',
+      load_breakpoints_event = { 'BufReadPost' },
+      perf_record = false,
+      on_load_breakpoint = nil,
+    }
+  }
 }
