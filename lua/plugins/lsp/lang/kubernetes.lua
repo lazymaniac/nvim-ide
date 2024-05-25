7fe31bb99adfde64d657a6e4c8bf5ec12df4e007
lazymaniac <sebastian.fabisz@gmail.com>
2024-05-25 09:37:10 +0100

    add new kube-utils-nvim plugin

---
+++ lua/plugins/lsp/lang/kubernetes.lua
@@ -0,0 +1,33 @@
+return {
+
+  -- [kube-utils-nvim] - Provides intehration with kubernetes and help
+  -- see: `:h kube-utils-nvim`
+  -- link: https://github.com/h4ckm1n-dev/kube-utils-nvim
+  {
+    "h4ckm1n-dev/kube-utils-nvim",
+    branch = "main", -- comment to use local folder
+    config = function()
+      require("init").setup()
+      local helm_mappings = {
+        k = {
+          name = "Kubernetes",
+          d = { "<cmd>HelmDeployFromBuffer<CR>", "Helm Deploy Buffer to Context" },
+          r = { "<cmd>RemoveDeployment<CR>", "Helm Remove Deployment From Buffer" },
+          T = { "<cmd>HelmDryRun<CR>", "Helm DryRun Buffer" },
+          a = { "<cmd>KubectlApplyFromBuffer<CR>", "Kubectl Apply From Buffer" },
+          D = { "<cmd>DeleteNamespace<CR>", "Kubectl Delete Namespace" },
+          u = { "<cmd>HelmDependencyUpdateFromBuffer<CR>", "Helm Dependency Update" },
+          b = { "<cmd>HelmDependencyBuildFromBuffer<CR>", "Helm Dependency Build" },
+          t = { "<cmd>HelmTemplateFromBuffer<CR>", "Helm Template From Buffer" },
+          K = { "<cmd>OpenK9sSplit<CR>", "Split View K9s" },
+          k = { "<cmd>OpenK9s<CR>", "Open K9s" },
+          l = { "<cmd>ToggleYamlHelm<CR>", "Toggle YAML/Helm" },
+        },
+      }
+
+      local wk = require 'which-key'
+      -- Register the Helm keybindings with a specific prefix
+      wk.register(helm_mappings, { prefix = "<leader>" })
+    end,
+  },
+}
