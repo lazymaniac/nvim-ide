4da005310d66e90a0979ee2489b827bd222fdf7f
lazymaniac <sebastian.fabisz@gmail.com>
2024-05-25 09:25:33 +0100

    add git dev for remote git repo peek

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -25,8 +25,8 @@ return {
         untracked = { text = '▎' },
       },
       signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-      numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
-      linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+      numhl = true,      -- Toggle with `:Gitsigns toggle_numhl`
+      linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
       word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
       watch_gitdir = {
         interval = 1000,
@@ -154,23 +154,24 @@ return {
     dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim', 'sindrets/diffview.nvim', 'stevearc/dressing.nvim', 'nvim-tree/nvim-web-devicons' },
     -- stylua: ignore
     keys = {
-      { '<leader>glr', function() require('gitlab').review() end, mode = { 'n' }, desc = 'Gitlab review [glr]' },
-      { '<leader>gls', function() require('gitlab').summary() end, mode = { 'n' }, desc = 'Gitlab summary [gls]' },
-      { '<leader>glA', function() require('gitlab').approve() end, mode = { 'n' }, desc = 'Gitlab approve [glA]' },
-      { '<leader>glR', function() require('gitlab').revoke() end, mode = { 'n' }, desc = 'Gitlab revoke [glR]' },
-      { '<leader>glc', function() require('gitlab').create_comment() end, mode = { 'n' }, desc = 'Gitlab create comment [glc]' },
-      { '<leader>glc', function() require('gitlab').create_multiline_comment() end, mode = { 'v' }, desc = 'Gitlab create multiline comment [glc]' },
-      { '<leader>glC', function() require('gitlab').create_comment_suggestion() end, mode = { 'v' }, desc = 'Gitlab create comment suggestion [glC]' },
-      { '<leader>glm', function() require('gitlab').move_to_discussion_tree_from_diagnostic() end, mode = { 'n' }, desc = 'Gitlab move to discussion tree from diagnostics [glm]' },
-      { '<leader>gln', function() require('gitlab').create_note() end, mode = { 'n' }, desc = 'Gitlab create note [gln]' },
-      { '<leader>gld', function() require('gitlab').toggle_discussions() end, mode = { 'n' }, desc = 'Gitlab toggle discussion [gld]' },
-      { '<leader>glPaa', function() require('gitlab').add_assignee() end, mode = { 'n' }, desc = 'Gitlab add assignee [glPaa]' },
-      { '<leader>glPad', function() require('gitlab').delete_assignee() end, mode = { 'n' }, desc = 'Gitlab delete assignee [glPad]' },
-      { '<leader>glPra', function() require('gitlab').add_reviewer() end, mode = { 'n' }, desc = 'Gitlab add reviewer [glPra]' },
-      { '<leader>glPrd', function() require('gitlab').delete_reviewer() end, mode = { 'n' }, desc = 'Gitlab delete reviewer [glPrd]' },
-      { '<leader>glp', function() require('gitlab').pipeline() end, mode = { 'n' }, desc = 'Gitlab pipeline [glp]' },
-      { '<leader>glo', function() require('gitlab').open_in_browser() end, mode = { 'n' }, desc = 'Gitlab open in browser [glo]' },
-      { '<leader>glB',
+      { '<leader>glr',   function() require('gitlab').review() end,                                  mode = { 'n' }, desc = 'Gitlab review [glr]' },
+      { '<leader>gls',   function() require('gitlab').summary() end,                                 mode = { 'n' }, desc = 'Gitlab summary [gls]' },
+      { '<leader>glA',   function() require('gitlab').approve() end,                                 mode = { 'n' }, desc = 'Gitlab approve [glA]' },
+      { '<leader>glR',   function() require('gitlab').revoke() end,                                  mode = { 'n' }, desc = 'Gitlab revoke [glR]' },
+      { '<leader>glc',   function() require('gitlab').create_comment() end,                          mode = { 'n' }, desc = 'Gitlab create comment [glc]' },
+      { '<leader>glc',   function() require('gitlab').create_multiline_comment() end,                mode = { 'v' }, desc = 'Gitlab create multiline comment [glc]' },
+      { '<leader>glC',   function() require('gitlab').create_comment_suggestion() end,               mode = { 'v' }, desc = 'Gitlab create comment suggestion [glC]' },
+      { '<leader>glm',   function() require('gitlab').move_to_discussion_tree_from_diagnostic() end, mode = { 'n' }, desc = 'Gitlab move to discussion tree from diagnostics [glm]' },
+      { '<leader>gln',   function() require('gitlab').create_note() end,                             mode = { 'n' }, desc = 'Gitlab create note [gln]' },
+      { '<leader>gld',   function() require('gitlab').toggle_discussions() end,                      mode = { 'n' }, desc = 'Gitlab toggle discussion [gld]' },
+      { '<leader>glPaa', function() require('gitlab').add_assignee() end,                            mode = { 'n' }, desc = 'Gitlab add assignee [glPaa]' },
+      { '<leader>glPad', function() require('gitlab').delete_assignee() end,                         mode = { 'n' }, desc = 'Gitlab delete assignee [glPad]' },
+      { '<leader>glPra', function() require('gitlab').add_reviewer() end,                            mode = { 'n' }, desc = 'Gitlab add reviewer [glPra]' },
+      { '<leader>glPrd', function() require('gitlab').delete_reviewer() end,                         mode = { 'n' }, desc = 'Gitlab delete reviewer [glPrd]' },
+      { '<leader>glp',   function() require('gitlab').pipeline() end,                                mode = { 'n' }, desc = 'Gitlab pipeline [glp]' },
+      { '<leader>glo',   function() require('gitlab').open_in_browser() end,                         mode = { 'n' }, desc = 'Gitlab open in browser [glo]' },
+      {
+        '<leader>glB',
         function()
           local gitlab = require 'gitlab'
           require('gitlab.server').restart(function()
@@ -184,21 +185,21 @@ return {
       { '<leader>glt', function() require('gitlab').print_settings() end, mode = { 'n' }, desc = 'Gitlab troubleshoot settings [glt]' },
     },
     opts = {
-      port = nil, -- The port of the Go server, which runs in the background, if omitted or `nil` the port will be chosen automatically
+      port = nil,                                              -- The port of the Go server, which runs in the background, if omitted or `nil` the port will be chosen automatically
       log_path = vim.fn.stdpath 'cache' .. '/gitlab.nvim.log', -- Log path for the Go server
-      config_path = nil, -- Custom path for `.gitlab.nvim` file, please read the "Connecting to Gitlab" section
-      debug = { go_request = false, go_response = false }, -- Which values to log
-      attachment_dir = nil, -- The local directory for files (see the "summary" section)
-      help = '?', -- Opens a help popup for local keymaps when a relevant view is focused (popup, discussion panel, etc)
-      popup = { -- The popup for comment creation, editing, and replying
+      config_path = nil,                                       -- Custom path for `.gitlab.nvim` file, please read the "Connecting to Gitlab" section
+      debug = { go_request = false, go_response = false },     -- Which values to log
+      attachment_dir = nil,                                    -- The local directory for files (see the "summary" section)
+      help = '?',                                              -- Opens a help popup for local keymaps when a relevant view is focused (popup, discussion panel, etc)
+      popup = {                                                -- The popup for comment creation, editing, and replying
         exit = '<Esc>',
-        perform_action = '<leader>s', -- Once in normal mode, does action (like saving comment or editing description, etc)
-        perform_linewise_action = '<leader>l', -- Once in normal mode, does the linewise action (see logs for this job, etc)
+        perform_action = '<leader>s',                          -- Once in normal mode, does action (like saving comment or editing description, etc)
+        perform_linewise_action = '<leader>l',                 -- Once in normal mode, does the linewise action (see logs for this job, etc)
         width = '40%',
         height = '60%',
         border = 'rounded', -- One of "rounded", "single", "double", "solid"
-        opacity = 1.0, -- From 0.0 (fully transparent) to 1.0 (fully opaque)
-        comment = nil, -- Individual popup overrides, e.g. { width = "60%", height = "80%", border = "single", opacity = 0.85 },
+        opacity = 1.0,      -- From 0.0 (fully transparent) to 1.0 (fully opaque)
+        comment = nil,      -- Individual popup overrides, e.g. { width = "60%", height = "80%", border = "single", opacity = 0.85 },
         edit = nil,
         note = nil,
         pipeline = nil,
@@ -225,10 +226,10 @@ return {
         winbar = nil, -- Custom function to return winbar title, should return a string. Provided with WinbarTable (defined in annotations.lua)
         -- If using lualine, please add "gitlab" to disabled file types, otherwise you will not see the winbar.
       },
-      info = { -- Show additional fields in the summary pane
+      info = {              -- Show additional fields in the summary pane
         enabled = true,
         horizontal = false, -- Display metadata to the left of the summary rather than underneath
-        fields = { -- The fields listed here will be displayed, in whatever order you choose
+        fields = {          -- The fields listed here will be displayed, in whatever order you choose
           'author',
           'created_at',
           'updated_at',
@@ -268,7 +269,7 @@ return {
         -- for namespace `gitlab_discussion`. See :h vim.diagnostic.config
         enabled = true,
         severity = vim.diagnostic.severity.INFO,
-        code = nil, -- see :h diagnostic-structure
+        code = nil,        -- see :h diagnostic-structure
         display_opts = {}, -- see opts in vim.diagnostic.set
       },
       pipeline = {
@@ -338,20 +339,20 @@ return {
     event = 'VeryLazy',
     -- stylua: ignore
     keys = {
-      { '<leader>gd', '<cmd>DiffviewOpen<cr>', mode = { 'n', 'v' }, desc = 'Open Diff View [gd]' },
-      { '<leader>gq', '<cmd>DiffviewClose<cr>', mode = { 'n', 'v' }, desc = 'Close Diff View [gq]' },
+      { '<leader>gd', '<cmd>DiffviewOpen<cr>',        mode = { 'n', 'v' }, desc = 'Open Diff View [gd]' },
+      { '<leader>gq', '<cmd>DiffviewClose<cr>',       mode = { 'n', 'v' }, desc = 'Close Diff View [gq]' },
       { '<leader>gf', '<cmd>DiffviewFileHistory<cr>', mode = { 'n', 'v' }, desc = 'Diff View File History [gf]' },
     },
     config = function()
       require('diffview').setup {
-        diff_binaries = false, -- Show diffs for binaries
+        diff_binaries = false,    -- Show diffs for binaries
         enhanced_diff_hl = false, -- See ':h diffview-config-enhanced_diff_hl'
-        git_cmd = { 'git' }, -- The git executable followed by default args.
-        hg_cmd = { 'hg' }, -- The hg executable followed by default args.
-        use_icons = true, -- Requires nvim-web-devicons
-        show_help_hints = true, -- Show hints for how to open the help panel
-        watch_index = true, -- Update views and index buffers when the git index changes.
-        icons = { -- Only applies when use_icons is true.
+        git_cmd = { 'git' },      -- The git executable followed by default args.
+        hg_cmd = { 'hg' },        -- The hg executable followed by default args.
+        use_icons = true,         -- Requires nvim-web-devicons
+        show_help_hints = true,   -- Show hints for how to open the help panel
+        watch_index = true,       -- Update views and index buffers when the git index changes.
+        icons = {                 -- Only applies when use_icons is true.
           folder_closed = '',
           folder_open = '',
         },
@@ -380,7 +381,7 @@ return {
             -- Config for conflicted files in diff views during a merge or rebase.
             layout = 'diff3_horizontal',
             disable_diagnostics = true, -- Temporarily disable diagnostics for conflict buffers while in the view.
-            winbar_info = true, -- See ':h diffview-config-view.x.winbar_info'
+            winbar_info = true,         -- See ':h diffview-config-view.x.winbar_info'
           },
           file_history = {
             -- Config for changed files in file history views.
@@ -389,12 +390,12 @@ return {
           },
         },
         file_panel = {
-          listing_style = 'tree', -- One of 'list' or 'tree'
-          tree_options = { -- Only applies when listing_style is 'tree'
-            flatten_dirs = true, -- Flatten dirs that only contain one single dir
+          listing_style = 'tree',            -- One of 'list' or 'tree'
+          tree_options = {                   -- Only applies when listing_style is 'tree'
+            flatten_dirs = true,             -- Flatten dirs that only contain one single dir
             folder_statuses = 'only_folded', -- One of 'never', 'only_folded' or 'always'.
           },
-          win_config = { -- See ':h diffview-config-win_config'
+          win_config = {                     -- See ':h diffview-config-win_config'
             position = 'left',
             width = 35,
             win_opts = {},
@@ -430,7 +431,7 @@ return {
           DiffviewOpen = {},
           DiffviewFileHistory = {},
         },
-        hooks = {}, -- See ':h diffview-config-hooks'
+        hooks = {},                 -- See ':h diffview-config-hooks'
         keymaps = {
           disable_defaults = false, -- Disable the default keymaps
         },
@@ -451,14 +452,14 @@ return {
       { '<leader>gi', '<cmd>Octo<cr>', desc = 'Github [gi]' },
     },
     opts = {
-      use_local_fs = false, -- use local files on right side of reviews
-      enable_builtin = true, -- shows a list of builtin actions when no action is provided
+      use_local_fs = false,                      -- use local files on right side of reviews
+      enable_builtin = true,                     -- shows a list of builtin actions when no action is provided
       default_remote = { 'upstream', 'origin' }, -- order to try remotes
-      ssh_aliases = {}, -- SSH aliases. e.g. `ssh_aliases = {["github.com-work"] = "github.com"}`
-      picker = 'telescope', -- or "fzf-lua"
+      ssh_aliases = {},                          -- SSH aliases. e.g. `ssh_aliases = {["github.com-work"] = "github.com"}`
+      picker = 'telescope',                      -- or "fzf-lua"
       picker_config = {
-        use_emojis = false, -- only used by "fzf-lua" picker for now
-        mappings = { -- mappings for the pickers
+        use_emojis = false,                      -- only used by "fzf-lua" picker for now
+        mappings = {                             -- mappings for the pickers
           open_in_browser = { lhs = '<C-b>', desc = 'Open Issue in Browser <C-b>' },
           copy_url = { lhs = '<C-y>', desc = 'Copy Crl to System Clipboard <C-y>' },
           checkout_pr = { lhs = '<C-o>', desc = 'Checkout Pull Request <C-o>' },
@@ -482,20 +483,20 @@ return {
         use_signcolumn = true, -- show "modified" marks on the sign column
       },
       issues = {
-        order_by = { -- criteria to sort results of `Octo issue list`
+        order_by = {            -- criteria to sort results of `Octo issue list`
           field = 'CREATED_AT', -- either COMMENTS, CREATED_AT or UPDATED_AT (https://docs.github.com/en/graphql/reference/enums#issueorderfield)
-          direction = 'DESC', -- either DESC or ASC (https://docs.github.com/en/graphql/reference/enums#orderdirection)
+          direction = 'DESC',   -- either DESC or ASC (https://docs.github.com/en/graphql/reference/enums#orderdirection)
         },
       },
       pull_requests = {
-        order_by = { -- criteria to sort the results of `Octo pr list`
-          field = 'CREATED_AT', -- either COMMENTS, CREATED_AT or UPDATED_AT (https://docs.github.com/en/graphql/reference/enums#issueorderfield)
-          direction = 'DESC', -- either DESC or ASC (https://docs.github.com/en/graphql/reference/enums#orderdirection)
+        order_by = {                            -- criteria to sort the results of `Octo pr list`
+          field = 'CREATED_AT',                 -- either COMMENTS, CREATED_AT or UPDATED_AT (https://docs.github.com/en/graphql/reference/enums#issueorderfield)
+          direction = 'DESC',                   -- either DESC or ASC (https://docs.github.com/en/graphql/reference/enums#orderdirection)
         },
         always_select_remote_on_create = false, -- always give prompt to select base remote repo when creating PRs
       },
       file_panel = {
-        size = 10, -- changed files panel rows
+        size = 10,        -- changed files panel rows
         use_icons = true, -- use web-devicons in file panel (if false, nvim-web-devicons does not need to be installed)
       },
     },
@@ -687,4 +688,10 @@ return {
       }
     end,
   },
+
+  {
+    "moyiz/git-dev.nvim",
+    event = "VeryLazy",
+    opts = {},
+  }
 }
