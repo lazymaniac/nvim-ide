121dc478de8ab36eb9f575fce9c6c6d0c98e89f0
lazymaniac <sebastian.fabisz@gmail.com>
2024-05-26 01:01:33 +0100

    new colorscheme

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -13,7 +13,7 @@ local defaults = {
       vim.cmd.colorscheme 'terafox'
     end
   end, ]]
-  colorscheme = 'terafox',
+  colorscheme = 'neofusion',
   -- load the default settings
   defaults = {
     autocmds = true, -- config.autocmds
