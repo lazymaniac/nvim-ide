dbe7332da940876320517d89d2f7b8f2ead356ab
lazymaniac <sebastian.fabisz@gmail.com>
2024-09-25 22:55:53 +0100

    code cleanup

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -153,45 +153,4 @@ return {
       }
     end,
   },
-
-  {
-    'loganswartz/sunburn.nvim',
-    branch = 'main',
-    dependencies = { 'loganswartz/polychrome.nvim' },
-    -- you could do this, or use the standard vimscript `colorscheme sunburn`
-    config = function() end,
-  },
-
-  {
-    'diegoulloao/neofusion.nvim',
-    priority = 1000,
-    config = true,
-    opts = {
-      terminal_colors = true, -- add neovim terminal colors
-      undercurl = true,
-      underline = true,
-      bold = true,
-      italic = {
-        strings = true,
-        emphasis = true,
-        comments = true,
-        operators = false,
-        folds = true,
-      },
-      strikethrough = true,
-      invert_selection = false,
-      invert_signs = false,
-      invert_tabline = false,
-      invert_intend_guides = false,
-      inverse = true, -- invert background for search, diffs, statuslines and errors
-      palette_overrides = {},
-      overrides = {},
-      dim_inactive = false,
-      transparent_mode = false,
-    },
-  },
-
-  {
-    'atmosuwiryo/vim-winteriscoming',
-  },
 }
