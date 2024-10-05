83b6fb169fd5a71aad6a0acd4089d262520429a8
lazymaniac <sebastian.fabisz@gmail.com>
2024-10-05 10:57:15 +0100

    remove wilder

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -231,13 +231,13 @@ return {
       { '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Buffer Diagnostics [xX]' },
       { '<leader>xL', '<cmd>Trouble loclist toggle<cr>', desc = 'Location List [xL]' },
       { '<leader>xQ', '<cmd>Trouble qflist toggle<cr>', desc = 'Quickfix List [xQ]' },
-      { '<leader>xs', '<cmd>Trouble symbols toggle win.relative=win win.position=right<cr>' },
-      { '<leader>xl', '<cmd>Trouble lsp toggle win.relative=win win.position=right<cr>' },
+      { '<leader>o', '<cmd>Trouble lsp_document_symbols toggle win.relative=win win.position=right<cr>', desc = "Toggle symbols [xs]" },
+      { '<leader>xl', '<cmd>Trouble lsp toggle win.relative=win win.position=right<cr>', desc = 'Toggle LSP [xl]' },
       {
         '[q',
         function()
           if require('trouble').is_open() then
-            require('trouble').previous { skip_groups = true, jump = true }
+            require('trouble').prev { skip_groups = true, jump = true }
           else
             local ok, err = pcall(vim.cmd.cprev)
             if not ok then
@@ -701,75 +701,6 @@ return {
         -- Please PR commonly ignored filetypes
         ignore_filetypes = { 'NvimTree', 'TelescopePrompt' },
       }
-    end,
-  },
-
-  -- [wilder.nvim] - cmdline improving plugin
-  -- see: `:h wilder.nvim`
-  -- link: https://github.com/gelguy/wilder.nvim
-  {
-    'gelguy/wilder.nvim',
-    branch = 'master',
-    config = function()
-      local wilder = require 'wilder'
-      wilder.setup { modes = { ':', '/', '?' } }
-      wilder.set_option('pipeline', {
-        wilder.branch(
-          wilder.cmdline_pipeline {
-            -- sets the language to use, 'vim' and 'python' are supported
-            language = 'python',
-            -- 0 turns off fuzzy matching
-            -- 1 turns on fuzzy matching
-            -- 2 partial fuzzy matching (match does not have to begin with the same first letter)
-            fuzzy = 1,
-          },
-          wilder.python_search_pipeline {
-            -- can be set to wilder#python_fuzzy_delimiter_pattern() for stricter fuzzy matching
-            pattern = wilder.python_fuzzy_pattern(),
-            -- omit to get results in the order they appear in the buffer
-            sorter = wilder.python_difflib_sorter(),
-            -- can be set to 're2' for performance, requires pyre2 to be installed
-            -- see :h wilder#python_search() for more details
-            engine = 're',
-          }
-        ),
-      })
-      local gradient = {
-        '#f4468f',
-        '#fd4a85',
-        '#ff507a',
-        '#ff566f',
-        '#ff5e63',
-        '#ff6658',
-        '#ff704e',
-        '#ff7a45',
-        '#ff843d',
-        '#ff9036',
-        '#f89b31',
-        '#efa72f',
-        '#e6b32e',
-        '#dcbe30',
-        '#d2c934',
-        '#c8d43a',
-        '#bfde43',
-        '#b6e84e',
-        '#aff05b',
-      }
-      for i, fg in ipairs(gradient) do
-        gradient[i] = wilder.make_hl('WilderGradient' .. i, 'Pmenu', { { a = 1 }, { a = 1 }, { foreground = fg } })
-      end
-      wilder.set_option(
-        'renderer',
-        wilder.popupmenu_renderer {
-          highlights = {
-            gradient = gradient, -- must be set
-            -- selected_gradient key can be set to apply gradient highlighting for the selected candidate.
-          },
-          highlighter = wilder.highlighter_with_gradient {
-            wilder.basic_highlighter(), -- or wilder.lua_fzy_highlighter(),
-          },
-        }
-      )
     end,
   },
 
