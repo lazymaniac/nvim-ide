9e56a1076d821848c56d70c4dc0ea1090ffca594
lazymaniac <sebastian.fabisz@gmail.com>
2024-10-12 20:28:12 +0100

    add conditional breakpoints

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -20,10 +20,10 @@ return {
     branch = 'master',
     event = 'VeryLazy',
     dependencies = {
-      { 'rcarriga/nvim-dap-ui',            branch = 'master' },
+      { 'rcarriga/nvim-dap-ui', branch = 'master' },
       { 'theHamsta/nvim-dap-virtual-text', branch = 'master' },
-      { 'jay-babu/mason-nvim-dap.nvim',    branch = 'main' },
-      { 'stevearc/overseer.nvim',          branch = 'master' },
+      { 'jay-babu/mason-nvim-dap.nvim', branch = 'main' },
+      { 'stevearc/overseer.nvim', branch = 'master' },
       { 'LiadOz/nvim-dap-repl-highlights', branch = 'master' },
     },
     -- stylua: ignore
@@ -59,8 +59,7 @@ return {
       vim.api.nvim_set_hl(0, 'DapStoppedLine', { default = true, link = 'Visual' })
       for name, sign in pairs(Config.icons.dap) do
         sign = type(sign) == 'table' and sign or { sign }
-        vim.fn.sign_define('Dap' .. name,
-          { text = sign[1], texthl = sign[2] or 'DiagnosticInfo', linehl = sign[3], numhl = sign[3] })
+        vim.fn.sign_define('Dap' .. name, { text = sign[1], texthl = sign[2] or 'DiagnosticInfo', linehl = sign[3], numhl = sign[3] })
       end
     end,
   },
@@ -124,11 +123,11 @@ return {
         layouts = {
           {
             elements = {
-              { id = 'scopes',      size = 0.2 },
-              { id = 'stacks',      size = 0.2 },
-              { id = 'watches',     size = 0.2 },
+              { id = 'scopes', size = 0.2 },
+              { id = 'stacks', size = 0.2 },
+              { id = 'watches', size = 0.2 },
               { id = 'breakpoints', size = 0.1 },
-              { id = 'repl',        size = 0.3 },
+              { id = 'repl', size = 0.3 },
             },
             position = 'right',
             size = 60,
@@ -172,15 +171,15 @@ return {
       { '<leader>dv', '<cmd>DapVirtualTextToggle<cr>', desc = 'Toggle DAP Virtual Text [dv]', mode = { 'n', 'v' } },
     },
     opts = {
-      enabled = true,                     -- enable this plugin (the default)
-      enabled_commands = true,            -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+      enabled = true, -- enable this plugin (the default)
+      enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
       highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-      highlight_new_as_changed = false,   -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-      show_stop_reason = true,            -- show stop reason when stopped for exceptions
-      commented = false,                  -- prefix virtual text with comment string
-      only_first_definition = false,      -- only show virtual text at first definition (if there are multiple)
-      all_references = true,              -- show virtual text on all all references of the variable (not only definitions)
-      clear_on_continue = false,          -- clear virtual text on "continue" (might cause flickering when stepping)
+      highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+      show_stop_reason = true, -- show stop reason when stopped for exceptions
+      commented = false, -- prefix virtual text with comment string
+      only_first_definition = false, -- only show virtual text at first definition (if there are multiple)
+      all_references = true, -- show virtual text on all all references of the variable (not only definitions)
+      clear_on_continue = false, -- clear virtual text on "continue" (might cause flickering when stepping)
       ---@diagnostic disable-next-line: unused-local
       display_callback = function(variable, buf, stackframe, node, options)
         if options.virt_text_pos == 'inline' then
@@ -192,8 +191,8 @@ return {
       -- position of virtual text, see `:h nvim_buf_set_extmark()`, default tries to inline the virtual text. Use 'eol' to set to end of line
       virt_text_pos = vim.fn.has 'nvim-0.10' == 1 and 'inline' or 'eol',
       -- experimental features:
-      all_frames = false,      -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-      virt_lines = false,      -- show virtual lines instead of virtual text (will flicker!)
+      all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+      virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
       virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
       -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
     },
@@ -244,14 +243,23 @@ return {
     branch = 'main',
     keys = {
       { '<leader>dB', '<cmd>PBSetConditionalBreakpoint<cr>', desc = 'Conditional Breakpoint [dB]' },
-      { '<leader>db', '<cmd>PBToggleBreakpoint<cr>',         desc = 'Toggle Breakpoint [db]' },
-      { '<leader>dx', '<cmd>PBClearAllBreakpoints<cr>',      desc = 'Clear all Breakpoints [dx]' },
+      { '<leader>db', '<cmd>PBToggleBreakpoint<cr>', desc = 'Toggle Breakpoint [db]' },
+      { '<leader>dx', '<cmd>PBClearAllBreakpoints<cr>', desc = 'Clear all Breakpoints [dx]' },
     },
     opts = {
-      save_dir = vim.fn.stdpath('cache') .. '/nvim_breakpoints',
+      save_dir = vim.fn.stdpath 'cache' .. '/nvim_breakpoints',
       load_breakpoints_event = { 'BufReadPost' },
       perf_record = false,
       on_load_breakpoint = nil,
-    }
-  }
+    },
+  },
+
+  {
+    'lucaSartore/nvim-dap-exception-breakpoints',
+    dependencies = { 'mfussenegger/nvim-dap' },
+    config = function()
+      local set_exception_breakpoints = require 'nvim-dap-exception-breakpoints'
+      vim.api.nvim_set_keymap('n', '<leader>dE', '', { desc = '[D]ebug [C]ondition breakpoints', callback = set_exception_breakpoints })
+    end,
+  },
 }
