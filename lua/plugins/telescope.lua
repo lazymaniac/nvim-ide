dd7f1906ae7249f7970138e02503c1e2d5197749
lazymaniac <sebastian.fabisz@gmail.com>
2024-10-14 23:33:35 +0100

    remove unused plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -203,36 +203,6 @@ return {
             override_file_sorter = true, -- override the file sorter
             case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
           },
-          lazy = {
-            -- Optional theme (the extension doesn't set a default theme)
-            theme = 'ivy',
-            -- Whether or not to show the icon in the first column
-            show_icon = true,
-            -- Mappings for the actions
-            mappings = {
-              open_in_browser = '<C-o>',
-              open_in_file_browser = '<M-b>',
-              open_in_find_files = '<C-f>',
-              open_in_live_grep = '<C-g>',
-              open_in_terminal = '<C-t>',
-              open_plugins_picker = '<C-b>', -- Works only after having called first another action
-              open_lazy_root_find_files = '<C-r>f',
-              open_lazy_root_live_grep = '<C-r>g',
-              change_cwd_to_plugin = '<C-c>d',
-            },
-            -- Configuration that will be passed to the window that hosts the terminal
-            -- For more configuration options check 'nvim_open_win()'
-            terminal_opts = {
-              relative = 'editor',
-              style = 'minimal',
-              border = 'rounded',
-              title = 'Telescope lazy',
-              title_pos = 'center',
-              width = 0.5,
-              height = 0.5,
-            },
-            -- Other telescope configuration options
-          },
         },
         cmdline = {
           picker = {
@@ -332,23 +302,6 @@ return {
     config = function()
       ---@diagnostic disable-next-line: undefined-field
       require('telescope').load_extension 'dap'
-    end,
-  },
-
-  -- [telescope-lazy] - Lazy integartion with telescope
-  -- see: `:h telescope-lazy`
-  -- link: https://github.com/tsakirist/telescope-lazy.nvim
-  {
-    'tsakirist/telescope-lazy.nvim',
-    branch = 'main',
-    dependencies = { 'nvim-telescope/telescope.nvim' },
-    -- stylua: ignore
-    keys = {
-      { '<leader>sl', '<cmd>Telescope lazy<cr>', mode = { 'n', 'v' }, desc = 'List Lazy Plugins [sl]' },
-    },
-    config = function()
-      ---@diagnostic disable-next-line: undefined-field
-      require('telescope').load_extension 'lazy'
     end,
   },
 
