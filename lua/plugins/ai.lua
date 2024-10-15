b986c1dac3fc0f21d02486c1f70508dff4c2e6ed
lazymaniac <sebastian.fabisz@gmail.com>
2024-10-15 06:04:17 +0100

    change api key loading

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -11,7 +11,7 @@ local config = {
     anthropic = function()
       return require('codecompanion.adapters').extend('anthropic', {
         env = {
-          api_key = 'cmd:echo $ANTHROPIC_API_KEY',
+          api_key = 'cmd:cat .anthropic',
         },
         schema = {
           max_tokens = {
