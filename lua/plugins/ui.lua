818091282b8ba7bd248bf706ab98b98798943915
lazymaniac <sebastian.fabisz@gmail.com>
2025-01-07 06:07:12 +0000

    new keymaps

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -78,7 +78,6 @@ return {
       { '<leader>gB', function() Snacks.gitbrowse() end, desc = 'Git Browse', },
       { '<leader>gb', function() Snacks.git.blame_line() end, desc = 'Git Blame Line', },
       { '<leader>gf', function() Snacks.lazygit.log_file() end, desc = 'Lazygit Current File History', },
-      { '<leader>gg', function() Snacks.lazygit() end, desc = 'Lazygit', },
       { '<leader>gl', function() Snacks.lazygit.log() end, desc = 'Lazygit Log (cwd)', },
       { '<leader>un', function() Snacks.notifier.hide() end, desc = 'Dismiss All Notifications', },
     },
