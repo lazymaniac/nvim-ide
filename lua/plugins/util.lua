9286dd50d9303e3d946b12c1836e7db19be4bf16
lazymaniac <sebastian.fabisz@gmail.com>
2025-01-23 21:23:33 +0100

    remove precognition

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -85,44 +85,6 @@ return {
     },
   },
 
-  -- [precognition.nvim] - Show hint with available movements.
-  -- see: `:h precognition.nvim`
-  -- link: https://github.com/tris203/precognition.nvim
-  {
-    'tris203/precognition.nvim',
-    branch = 'main',
-    event = 'VeryLazy',
-    keys = {
-      { '<leader>lh', '<cmd>lua require("precognition").toggle()<cr>', mode = { 'n', 'v' }, desc = 'Show movement hints [lh]' },
-    },
-    opts = {
-      startVisible = true,
-      showBlankVirtLine = true,
-      highlightColor = { link = 'Comment' },
-      hints = {
-        Caret = { text = '^', prio = 2 },
-        Dollar = { text = '$', prio = 1 },
-        MatchingPair = { text = '%', prio = 5 },
-        Zero = { text = '0', prio = 1 },
-        w = { text = 'w', prio = 10 },
-        b = { text = 'b', prio = 9 },
-        e = { text = 'e', prio = 8 },
-        W = { text = 'W', prio = 7 },
-        B = { text = 'B', prio = 6 },
-        E = { text = 'E', prio = 5 },
-      },
-      gutterHints = {
-        G = { text = 'G', prio = 10 },
-        gg = { text = 'gg', prio = 9 },
-        PrevParagraph = { text = '{', prio = 8 },
-        NextParagraph = { text = '}', prio = 8 },
-      },
-      disabled_fts = {
-        'startify',
-      },
-    },
-  },
-
   -- [[ TOYS ]] ---------------------------------------------------------------
 
   -- [wttr.nvim] - Show current weather in lualine or forecast in popup
