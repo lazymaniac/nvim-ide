d9f76944b1015e995008274faadc8b0d2f6c7def
lazymaniac <sebastian.fabisz@gmail.com>
2025-02-01 18:27:07 +0100

    add output-panel

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -17,6 +17,26 @@ return {
   },
 
   {
+    'mhanberg/output-panel.nvim',
+    version = '*',
+    event = 'VeryLazy',
+    config = function()
+      require('output_panel').setup {
+        max_buffer_size = 5000, -- default
+      }
+    end,
+    cmd = { 'OutputPanel' },
+    keys = {
+      {
+        '<leader>lo',
+        vim.cmd.OutputPanel,
+        mode = 'n',
+        desc = 'Toggle the output panel',
+      },
+    },
+  },
+
+  {
     'Chaitanyabsprip/fastaction.nvim',
     opts = {
       dismiss_keys = { 'j', 'k', '<c-c>', 'q' },
