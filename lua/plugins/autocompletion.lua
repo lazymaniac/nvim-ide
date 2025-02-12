fee55c03b233021a94f95bed2d44cfec6934fc70
lazymaniac <sebastian.fabisz@gmail.com>
2025-02-12 14:32:38 +0100

    small fixes

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -1,3 +1,34 @@
+local cmp_actions = {
+  -- Native Snippets
+  snippet_forward = function()
+    if vim.snippet.active { direction = 1 } then
+      vim.schedule(function()
+        vim.snippet.jump(1)
+      end)
+      return true
+    end
+  end,
+  snippet_stop = function()
+    if vim.snippet then
+      vim.snippet.stop()
+    end
+  end,
+}
+
+local function map(actions, fallback)
+  return function()
+    for _, name in ipairs(actions) do
+      if cmp_actions[name] then
+        local ret = cmp_actions[name]()
+        if ret then
+          return true
+        end
+      end
+    end
+    return type(fallback) == 'function' and fallback() or fallback
+  end
+end
+
 return {
 
   -- [[ AUTOCOMPLETION ]] ---------------------------------------------------------------
@@ -7,13 +38,13 @@ return {
     dependencies = {
       'rafamadriz/friendly-snippets',
       'mikavilpas/blink-ripgrep.nvim',
+      {
+        'saghen/blink.compat',
+        opts = {},
+      },
     },
-    -- use a release tag to download pre-built binaries
     version = '*',
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
+    event = 'InsertEnter',
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
@@ -26,7 +57,7 @@ return {
         -- Sets the fallback highlight groups to nvim-cmp's highlight groups
         -- Useful for when your theme doesn't support blink.cmp
         -- Will be removed in a future release
-        use_nvim_cmp_as_default = true,
+        use_nvim_cmp_as_default = false,
         -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
         -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = 'mono',
@@ -51,13 +82,14 @@ return {
               { 'label', 'label_description', gap = 1 },
               { 'kind_icon', 'kind' },
             },
+            treesitter = { 'lsp' },
           },
           border = 'rounded',
         },
         -- Show documentation when selecting a completion item
         documentation = {
           auto_show = true,
-          auto_show_delay_ms = 500,
+          auto_show_delay_ms = 200,
           window = {
             border = 'rounded',
           },
@@ -73,7 +105,9 @@ return {
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
+        compat = {},
         default = { 'lsp', 'path', 'snippets', 'buffer', 'ripgrep' },
+        cmdline = {},
         providers = {
           -- 👇🏻👇🏻 add the ripgrep provider config below
           ripgrep = {
@@ -162,5 +196,64 @@ return {
       },
     },
     opts_extend = { 'sources.default' },
+    config = function(_, opts)
+      -- setup compat sources
+      local enabled = opts.sources.default
+      for _, source in ipairs(opts.sources.compat or {}) do
+        opts.sources.providers[source] = vim.tbl_deep_extend('force', { name = source, module = 'blink.compat.source' }, opts.sources.providers[source] or {})
+        if type(enabled) == 'table' and not vim.tbl_contains(enabled, source) then
+          table.insert(enabled, source)
+        end
+      end
+
+      -- add ai_accept to <Tab> key
+      if not opts.keymap['<Tab>'] then
+        if opts.keymap.preset == 'super-tab' then -- super-tab
+          opts.keymap['<Tab>'] = {
+            require('blink.cmp.keymap.presets')['super-tab']['<Tab>'][1],
+            map { 'snippet_forward', 'ai_accept' },
+            'fallback',
+          }
+        else -- other presets
+          opts.keymap['<Tab>'] = {
+            map { 'snippet_forward', 'ai_accept' },
+            'fallback',
+          }
+        end
+      end
+
+      -- Unset custom prop to pass blink.cmp validation
+      opts.sources.compat = nil
+
+      -- check if we need to override symbol kinds
+      for _, provider in pairs(opts.sources.providers or {}) do
+        ---@cast provider blink.cmp.SourceProviderConfig|{kind?:string}
+        if provider.kind then
+          local CompletionItemKind = require('blink.cmp.types').CompletionItemKind
+          local kind_idx = #CompletionItemKind + 1
+
+          CompletionItemKind[kind_idx] = provider.kind
+          ---@diagnostic disable-next-line: no-unknown
+          CompletionItemKind[provider.kind] = kind_idx
+
+          ---@type fun(ctx: blink.cmp.Context, items: blink.cmp.CompletionItem[]): blink.cmp.CompletionItem[]
+          local transform_items = provider.transform_items
+          ---@param ctx blink.cmp.Context
+          ---@param items blink.cmp.CompletionItem[]
+          provider.transform_items = function(ctx, items)
+            items = transform_items and transform_items(ctx, items) or items
+            for _, item in ipairs(items) do
+              item.kind = kind_idx or item.kind
+            end
+            return items
+          end
+
+          -- Unset custom prop to pass blink.cmp validation
+          provider.kind = nil
+        end
+      end
+
+      require('blink.cmp').setup(opts)
+    end,
   },
 }
