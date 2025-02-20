e8889b23171f91de7b0ae705ef83bb84e4095cd2
lazymaniac <sebastian.fabisz@gmail.com>
2025-02-20 17:44:16 +0100

    git files slash commnad for codecompanion, fix blink config, add macro recording plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -388,7 +388,7 @@ return {
           enabled = true,
         },
       }
-      vim.api.nvim_set_hl(0, "NeominimapBorder", { link = "SnacksNotifierBorderInfo", default = true })
+      vim.api.nvim_set_hl(0, 'NeominimapBorder', { link = 'SnacksNotifierBorderInfo', default = true })
     end,
   },
 
@@ -452,5 +452,58 @@ return {
     config = function()
       require('hlslens').setup {}
     end,
+  },
+
+  {
+    'chrisgrieser/nvim-recorder',
+    opts = {
+      slots = { 'a', 'b' },
+
+      mapping = {
+        startStopRecording = 'q',
+        playMacro = 'Q',
+        switchSlot = '<C-q>',
+        editMacro = 'cq',
+        deleteAllMacros = 'dq',
+        yankMacro = 'yq',
+        -- ⚠️ this should be a string you don't use in insert mode during a macro
+        addBreakPoint = '##',
+      },
+
+      -- Clears all macros-slots on startup.
+      clear = false,
+
+      -- Log level used for non-critical notifications; mostly relevant for nvim-notify.
+      -- (Note that by default, nvim-notify does not show the levels `trace` & `debug`.)
+      logLevel = vim.log.levels.INFO, -- :help vim.log.levels
+
+      -- If enabled, only essential notifications are sent.
+      -- If you do not use a plugin like nvim-notify, set this to `true`
+      -- to remove otherwise annoying messages.
+      lessNotifications = false,
+
+      -- Use nerdfont icons in the status bar components and keymap descriptions
+      useNerdfontIcons = true,
+
+      -- Performance optimizations for macros with high count. When `playMacro` is
+      -- triggered with a count higher than the threshold, nvim-recorder
+      -- temporarily changes changes some settings for the duration of the macro.
+      performanceOpts = {
+        countThreshold = 100,
+        lazyredraw = true, -- enable lazyredraw (see `:h lazyredraw`)
+        noSystemClipboard = true, -- remove `+`/`*` from clipboard option
+        autocmdEventsIgnore = { -- temporarily ignore these autocmd events
+          'TextChangedI',
+          'TextChanged',
+          'InsertLeave',
+          'InsertEnter',
+          'InsertCharPre',
+        },
+      },
+
+      -- [experimental] partially share keymaps with nvim-dap.
+      -- (See README for further explanations.)
+      dapSharedKeymaps = false,
+    },
   },
 }
