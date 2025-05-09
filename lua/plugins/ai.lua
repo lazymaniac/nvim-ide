a48ddf0b9a931bbbc7b3d0d8cbd3d35e64502a34
Sebastian Fabisz <sebastian@MacBook-Pro-Sebastian.local>
2025-05-09 07:55:54 +0200

    remove(minuet): remove minuet ai

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -735,49 +735,4 @@ return {
       require('codecompanion').setup(config)
     end,
   },
-
-  {
-    'milanglacier/minuet-ai.nvim',
-    config = function()
-      require('minuet').setup {
-        virtualtext = {
-          auto_trigger_ft = { '*' },
-          keymap = {
-            -- accept whole completion
-            accept = '<A-A>',
-            -- accept one line
-            accept_line = '<A-a>',
-            -- accept n lines (prompts for number)
-            -- e.g. "A-z 2 CR" will accept 2 lines
-            accept_n_lines = '<A-z>',
-            -- Cycle to prev completion item, or manually invoke completion
-            prev = '<A-[>',
-            -- Cycle to next completion item, or manually invoke completion
-            next = '<A-]>',
-            dismiss = '<A-e>',
-          },
-        },
-        provider = 'openai_fim_compatible',
-        n_completions = 1, -- recommend for local model for resource saving
-        -- I recommend beginning with a small context window size and incrementally
-        -- expanding it, depending on your local computing power. A context window
-        -- of 512, serves as an good starting point to estimate your computing
-        -- power. Once you have a reliable estimate of your local computing power,
-        -- you should adjust the context window to a larger value.
-        context_window = 1024,
-        provider_options = {
-          openai_fim_compatible = {
-            api_key = 'TERM',
-            name = 'Ollama',
-            end_point = 'http://localhost:11434/v1/completions',
-            model = 'qwen2.5-coder:32b',
-            optional = {
-              max_tokens = 100,
-              top_p = 0.9,
-            },
-          },
-        },
-      }
-    end,
-  },
 }
