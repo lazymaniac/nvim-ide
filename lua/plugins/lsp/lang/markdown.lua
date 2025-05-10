8bdd71590f9b48834dd5b069c82c8bed446569f1
Sebastian Fabisz <sebastian@MacBook-Pro-Sebastian.local>
2025-05-10 23:50:04 +0200

    feat(markdown): replace glow with render-markdown

--- lua/plugins/lsp/lang/markdown.lua
+++ lua/plugins/lsp/lang/markdown.lua
@@ -11,16 +11,16 @@ return {
       -- Disable all keymaps by setting mappings field to 'false'.
       -- Selectively disable keymaps by setting corresponding field to 'false'.
       mappings = {
-        inline_surround_toggle = 'gs',       -- (string|boolean) toggle inline style
+        inline_surround_toggle = 'gs', -- (string|boolean) toggle inline style
         inline_surround_toggle_line = 'gss', -- (string|boolean) line-wise toggle inline style
-        inline_surround_delete = 'ds',       -- (string|boolean) delete emphasis surrounding cursor
-        inline_surround_change = 'cs',       -- (string|boolean) change emphasis surrounding cursor
-        link_add = 'gl',                     -- (string|boolean) add link
-        link_follow = 'gx',                  -- (string|boolean) follow link
-        go_curr_heading = ']c',              -- (string|boolean) set cursor to current section heading
-        go_parent_heading = ']p',            -- (string|boolean) set cursor to parent section heading
-        go_next_heading = ']]',              -- (string|boolean) set cursor to next section heading
-        go_prev_heading = '[[',              -- (string|boolean) set cursor to previous section heading
+        inline_surround_delete = 'ds', -- (string|boolean) delete emphasis surrounding cursor
+        inline_surround_change = 'cs', -- (string|boolean) change emphasis surrounding cursor
+        link_add = 'gl', -- (string|boolean) add link
+        link_follow = 'gx', -- (string|boolean) follow link
+        go_curr_heading = ']c', -- (string|boolean) set cursor to current section heading
+        go_parent_heading = ']p', -- (string|boolean) set cursor to parent section heading
+        go_next_heading = ']]', -- (string|boolean) set cursor to next section heading
+        go_prev_heading = '[[', -- (string|boolean) set cursor to previous section heading
       },
       inline_surround = {
         -- For the emphasis, strong, strikethrough, and code fields:
@@ -101,25 +101,8 @@ return {
     end,
   },
 
-  -- [glow.nvim] - Markdown preview in terminal
-  -- see: `:h glow.nvim`
-  -- link: https://github.com/ellisonleao/glow.nvim
   {
-    'ellisonleao/glow.nvim',
-    branch = 'main',
-    -- stylua: ignore
-    keys = {
-      { '<leader>cP', ft = 'markdown', '<cmd>Glow<cr>', desc = 'Markdown Preview (TUI) [cP]' },
-    },
-    cmd = 'Glow',
-    config = function()
-      require('glow').setup {
-        border = 'rounded', -- floating window border config
-        width = 150,
-        height = 100,
-        width_ratio = 0.8, -- maximum width of the Glow window compared to the nvim window size (overrides `width`)
-        height_ratio = 0.8,
-      }
-    end,
+    'MeanderingProgrammer/render-markdown.nvim',
+    ft = { 'markdown', 'codecompanion' },
   },
 }
