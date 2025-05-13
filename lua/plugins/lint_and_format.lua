8431abe7376b7bfd23dba1ed95032fd1d401c64d
Sebastian Fabisz <sebastian@MacBook-Pro-Sebastian.local>
2025-05-13 21:14:15 +0200

    fix(sonarlint): move sonarlint plugin to lint_and_format

--- lua/plugins/lint_and_format.lua
+++ lua/plugins/lint_and_format.lua
@@ -44,6 +44,46 @@ return {
     end,
   },
 
+  -- [sonarlint.nvim] - Sonarlint LSP
+  -- see: `:h sonarlint.nvim`
+  -- link: https://gitlab.com/schrieveslaach/sonarlint.nvim
+  {
+    'https://gitlab.com/schrieveslaach/sonarlint.nvim',
+    ft = { 'java', 'javascript', 'typescript', 'c', 'go', 'kubernetes', 'css', 'docker', 'xml', 'html', 'python' },
+    config = function()
+      require('sonarlint').setup {
+        server = {
+          cmd = {
+            'sonarlint-language-server',
+            '-stdio',
+            '-analyzers',
+            vim.fn.expand '$MASON/share/sonarlint-analyzers/sonarjava.jar',
+            vim.fn.expand '$MASON/share/sonarlint-analyzers/sonarjs.jar',
+            vim.fn.expand '$MASON/share/sonarlint-analyzers/sonarxml.jar',
+            vim.fn.expand '$MASON/share/sonarlint-analyzers/sonargo.jar',
+            vim.fn.expand '$MASON/share/sonarlint-analyzers/sonarhtml.jar',
+            vim.fn.expand '$MASON/share/sonarlint-analyzers/sonariac.jar',
+            vim.fn.expand '$MASON/share/sonarlint-analyzers/sonarjavasymbolicexecution.jar',
+            vim.fn.expand '$MASON/share/sonarlint-analyzers/sonarpython.jar',
+          },
+        },
+        filetypes = {
+          'java',
+          'javascript',
+          'typescript',
+          'c',
+          'go',
+          'kubernetes',
+          'css',
+          'docker',
+          'xml',
+          'html',
+          'python',
+        },
+      }
+    end,
+  },
+
   -- [[ FORMATTING ]] ---------------------------------------------------------------
 
   -- [conform.nvim] - Code formatting.
