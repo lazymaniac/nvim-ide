56fec40b9335c7f5b9b52dfe267771790e17991d
Sebastian Fabisz <sebastian@MacBook-Pro-Sebastian.local>
2025-06-06 22:50:19 +0200

    fix(copilot): config was nested

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -762,49 +762,57 @@ return {
     event = 'InsertEnter',
     config = function()
       require('copilot').setup {
-        {
-          panel = {
-            enabled = false,
-            auto_refresh = false,
-            keymap = {
-              jump_prev = '[[',
-              jump_next = ']]',
-              accept = '<CR>',
-              refresh = 'gr',
-              open = '<M-CR>',
-            },
-            layout = {
-              position = 'bottom', -- | top | left | right | horizontal | vertical
-              ratio = 0.4,
+        server_opts_overrides = {
+          settings = {
+            telemetry = {
+              telemetryLevel = 'off',
             },
           },
-          suggestion = {
-            enabled = true,
-            auto_trigger = false,
-            hide_during_completion = true,
-            debounce = 75,
-            trigger_on_accept = true,
-            keymap = {
-              accept = '<M-l>',
-              accept_word = false,
-              accept_line = false,
-              next = '<M-]>',
-              prev = '<M-[>',
-              dismiss = '<C-]>',
-            },
+        },
+        panel = {
+          enabled = false,
+          auto_refresh = false,
+          keymap = {
+            jump_prev = '[[',
+            jump_next = ']]',
+            accept = '<CR>',
+            refresh = 'gr',
+            open = '<M-CR>',
           },
-          filetypes = {
-            yaml = false,
-            markdown = false,
-            help = false,
-            gitcommit = false,
-            gitrebase = false,
-            hgcommit = false,
-            svn = false,
-            cvs = false,
-            ['.'] = false,
+          layout = {
+            position = 'bottom', -- | top | left | right | horizontal | vertical
+            ratio = 0.4,
           },
-          copilot_model = '',
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = false,
+          hide_during_completion = true,
+          debounce = 75,
+          trigger_on_accept = true,
+          keymap = {
+            accept = '<M-l>',
+            accept_word = false,
+            accept_line = false,
+            next = '<M-]>',
+            prev = '<M-[>',
+            dismiss = '<C-]>',
+          },
+        },
+        filetypes = {
+          yaml = false,
+          markdown = false,
+          help = false,
+          gitcommit = false,
+          gitrebase = false,
+          hgcommit = false,
+          svn = false,
+          cvs = false,
+          ['.'] = false,
+        },
+        copilot_model = '',
+        telemetry = {
+          telemetryLevel = 'off',
         },
       }
     end,
