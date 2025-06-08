0796c8490cd7599af74cc3e72ce20500f08c09c7
Sebastian Fabisz <sebastian@MacBook-Pro-Sebastian.local>
2025-06-08 22:10:12 +0200

    feat(lsp): add border to hover action

--- lua/plugins/lsp/keymaps.lua
+++ lua/plugins/lsp/keymaps.lua
@@ -1,5 +1,15 @@
 local M = {}
 
+local hover = vim.lsp.buf.hover
+---@diagnostic disable-next-line: duplicate-set-field
+vim.lsp.buf.hover = function()
+    return hover({
+        max_width = 100,
+        max_height = 14,
+        border = 'rounded',
+    })
+end
+
 M._keys = nil
 
 function M.get()
