76a02afd47a7a485dde37dc7f547ed0d266125ae
Sebastian Fabisz <sebastian@MacBook-Pro-Sebastian.local>
2025-06-08 22:12:24 +0200

    refactor: code cleanup

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1567,7 +1567,7 @@ return {
       { '<leader>o', function() Snacks.picker.lsp_symbols() end, desc = 'LSP Symbols [o]', },
       { '<leader>cw', function() Snacks.picker.lsp_workspace_symbols() end, desc = 'LSP Workspace Symbols [cw]', },
       -- Other
-      { '<leader>Z', function() Snacks.zen() end, desc = 'Toggle Zen Mode [Z]', },
+      { '<leader>z', function() Snacks.zen() end, desc = 'Toggle Zen Mode [z]', },
       { '<leader>.', function() Snacks.scratch() end, desc = 'Toggle Scratch Buffer [.]', },
       { '<leader>S', function() Snacks.scratch.select() end, desc = 'Select Scratch Buffer [S]', },
       { '<leader>mn', function() Snacks.notifier.show_history() end, desc = 'Notification History [sn]', },
