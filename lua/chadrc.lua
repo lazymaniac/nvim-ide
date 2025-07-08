33f5f349740deb3751fac76aafbcb39d7ad09c8e
Sebastian Fabisz <sebastian@MacBook-Pro-Sebastian.local>
2025-07-08 08:17:13 +0200

    update: remove lint from status line

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -25,19 +25,12 @@ return {
       -- default/round/block/arrow separators work only for default statusline theme
       -- round and block will work for minimal theme only
       separator_style = 'round',
-      order = { 'mode', 'file', 'git', '%=', '%=', 'diagnostics', 'dap', 'lint', 'lsp', 'cwd', 'cursor' },
+      order = { 'mode', 'file', 'git', '%=', '%=', 'diagnostics', 'dap', 'lsp', 'cwd', 'cursor' },
       modules = {
         dap = function()
           if require('dap').status() ~= '' then
-            return '  ' .. require('dap').status()
+            return '   ' .. require('dap').status() .. '  '
           end
-        end,
-        lint = function()
-          local linters = require('lint').get_running()
-          if #linters == 0 then
-            return '󰦕'
-          end
-          return '󱉶 ' .. table.concat(linters, ', ')
         end,
       },
     },
