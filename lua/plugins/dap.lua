e8d608986dc5ed649e966409f07ed4c416bb5081
lazymaniac <lazymaniac@users.noreply.github.com>
2026-02-21 21:17:40 +0100

    update: restore old nvim dap layout

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -67,36 +67,13 @@ return {
             },
           },
           windows = {
-            -- `prev` is the last used position, might be nil
-            position = function(prev)
-              local wins = vim.api.nvim_tabpage_list_wins(0)
-
-              -- Restores previous position if terminal is visible
-              if vim.iter(wins):find(function(win)
-                return vim.w[win].dapview_win_term
-              end) then
-                return prev
-              end
-
-              return vim.tbl_count(vim
-                .iter(wins)
-                :filter(function(win)
-                  local buf = vim.api.nvim_win_get_buf(win)
-                  local valid_buftype = vim.tbl_contains({ '', 'help', 'prompt', 'quickfix', 'terminal' }, vim.bo[buf].buftype)
-                  local dapview_win = vim.w[win].dapview_win or vim.w[win].dapview_win_term
-                  return valid_buftype and not dapview_win
-                end)
-                :totable()) > 1 and 'below' or 'right'
-            end,
-            size = function(pos)
-              return pos == 'below' and 0.25 or 0.5
-            end,
+            size = 0.25,
+            position = 'below',
             terminal = {
-              -- `pos` is the position for the regular window
-              position = function(pos)
-                return pos == 'below' and 'right' or 'below'
-              end,
               size = 0.5,
+              position = 'left',
+              -- List of debug adapters for which the terminal should be ALWAYS hidden
+              hide = {},
             },
           },
           help = {
