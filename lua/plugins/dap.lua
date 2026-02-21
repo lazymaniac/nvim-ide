35fec97acf1bdeacbc436e5b40135d9a627670cd
lazymaniac <lazymaniac@users.noreply.github.com>
2026-02-21 20:14:20 +0100

    chore: update dap plugins config

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -34,7 +34,20 @@ return {
             -- You can add a "console" section to merge the terminal with the other views
             sections = { 'watches', 'scopes', 'exceptions', 'breakpoints', 'threads', 'repl' },
             -- Must be one of the sections declared above (except for "console")
-            default_section = 'watches',
+            default_section = 'watcher',
+            show_keymap_hints = true,
+            base_sections = {
+              -- Labels can be set dynamically with functions
+              -- Each function receives the window's width and the current section as arguments
+              breakpoints = { label = 'Breakpoints', keymap = 'B' },
+              scopes = { label = 'Scopes', keymap = 'S' },
+              exceptions = { label = 'Exceptions', keymap = 'E' },
+              watches = { label = 'Watches', keymap = 'W' },
+              threads = { label = 'Threads', keymap = 'T' },
+              repl = { label = 'REPL', keymap = 'R' },
+              sessions = { label = 'Sessions', keymap = 'K' },
+              console = { label = 'Console', keymap = 'C' },
+            },
             -- Add your own sections
             custom_sections = {},
             controls = {
@@ -53,12 +66,89 @@ return {
               custom_buttons = {},
             },
           },
+          windows = {
+            -- `prev` is the last used position, might be nil
+            position = function(prev)
+              local wins = vim.api.nvim_tabpage_list_wins(0)
+
+              -- Restores previous position if terminal is visible
+              if vim.iter(wins):find(function(win)
+                return vim.w[win].dapview_win_term
+              end) then
+                return prev
+              end
+
+              return vim.tbl_count(vim
+                .iter(wins)
+                :filter(function(win)
+                  local buf = vim.api.nvim_win_get_buf(win)
+                  local valid_buftype = vim.tbl_contains({ '', 'help', 'prompt', 'quickfix', 'terminal' }, vim.bo[buf].buftype)
+                  local dapview_win = vim.w[win].dapview_win or vim.w[win].dapview_win_term
+                  return valid_buftype and not dapview_win
+                end)
+                :totable()) > 1 and 'below' or 'right'
+            end,
+            size = function(pos)
+              return pos == 'below' and 0.25 or 0.5
+            end,
+            terminal = {
+              -- `pos` is the position for the regular window
+              position = function(pos)
+                return pos == 'below' and 'right' or 'below'
+              end,
+              size = 0.5,
+            },
+          },
           help = {
             border = nil,
           },
+          render = {
+            -- Optionally a function that takes two `dap.Variable`'s as arguments
+            -- and is forwarded to a `table.sort` when rendering variables in the scopes view
+            sort_variables = nil,
+            -- Full control of how frames are rendered, see the "Custom Formatting" page
+            threads = {
+              -- Choose which items to display and how
+              format = function(name, lnum, path)
+                return {
+                  { part = name, separator = ' ' },
+                  { part = path, hl = 'FileName', separator = ':' },
+                  { part = lnum, hl = 'LineNumber' },
+                }
+              end,
+              -- Align columns
+              align = false,
+            },
+            -- Full control of how breakpoints are rendered, see the "Custom Formatting" page
+            breakpoints = {
+              -- Choose which items to display and how
+              format = function(line, lnum, path)
+                return {
+                  { part = path, hl = 'FileName' },
+                  { part = lnum, hl = 'LineNumber' },
+                  { part = line, hl = true },
+                }
+              end,
+              -- Align columns
+              align = false,
+            },
+          },
           -- Controls how to jump when selecting a breakpoint or navigating the stack
-          switchbuf = 'usetab',
+          -- Comma separated list, like the built-in 'switchbuf'. See :help 'switchbuf'
+          -- Only a subset of the options is available: newtab, useopen, usetab and uselast
+          -- Can also be a function that takes the current winnr and the bufnr that will jumped to
+          -- If a function, should return the winnr of the destination window
+          switchbuf = 'usetab,uselast',
+          -- Auto open when a session is started and auto close when all sessions finish
+          -- Alternatively, can be a string:
+          -- - "keep_terminal": as above, but keeps the terminal when the session finishes
+          -- - "open_term": open the terminal when starting a new session, nothing else
           auto_toggle = false,
+          -- Reopen dapview when switching to a different tab
+          -- Can also be a function to dynamically choose when to follow, by returning a boolean
+          -- If a function, receives the name of the adapter for the current session as an argument
+          follow_tab = false,
+          -- Controls how to jump when selecting a breakpoint or navigating the stack
         },
       },
       -- [nvim-dap-virtual-text] - Virtual text for debbug session
@@ -190,6 +280,7 @@ return {
       load_breakpoints_event = { 'BufReadPost' },
       perf_record = false,
       on_load_breakpoint = nil,
+      always_reload = true,
     },
   },
 
